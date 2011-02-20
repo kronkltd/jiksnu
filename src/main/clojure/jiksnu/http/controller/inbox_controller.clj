87aff07edd0ac63ef08595d2a2a596211fa4fc72
Daniel E. Renfer <duck@kronkltd.net>
2011-02-20 13:15:53 -0500

    inbox controller

---
+++ src/main/clojure/jiksnu/http/controller/inbox_controller.clj
@@ -0,0 +1,12 @@
+(ns jiksnu.http.controller.inbox-controller
+  (:use jiksnu.model)
+  (:require [jiksnu.model.activity :as model.activity]
+            [jiksnu.model.item :as model.item]
+            [jiksnu.model.user :as model.user] )
+  (:import jiksnu.model.Activity
+           jiksnu.model.User))
+
+(defn index
+  [request]
+  (let [user (model.user/show ((:params request) "username"))]
+    (model.item/fetch-activities user)))
