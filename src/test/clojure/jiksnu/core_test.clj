cc41e60a4cb6f3b8224758cd9e1e799a10d20ddf
Daniel E. Renfer <duck@kronkltd.net>
2011-02-27 00:14:37 -0500

    hooke

--- src/test/clojure/jiksnu/core_test.clj
+++ src/test/clojure/jiksnu/core_test.clj
@@ -1,7 +1,17 @@
 (ns jiksnu.core-test
   (:use jiksnu.core
+        jiksnu.model
+        robert.hooke
         [lazytest.describe :only (describe do-it testing)]))
 
 (describe start)
 
 (describe -main)
+
+(defn env-hook
+  [f & args]
+  (with-environment :test
+    (apply f args)))
+
+(add-hook #'lazytest.test-case/try-test-case env-hook)
+
--- src/test/clojure/jiksnu/model/activity_test.clj
+++ src/test/clojure/jiksnu/model/activity_test.clj
@@ -7,7 +7,8 @@
         jiksnu.session
         [lazytest.expect :only (expect)])
   (:require [karras.entity :as entity]
-            [jiksnu.model.user :as model.user])
+            [jiksnu.model.user :as model.user]
+            jiksnu.core-test)
   (:import jiksnu.model.Activity
            jiksnu.model.User))
 
@@ -47,97 +48,87 @@
 (describe create
   (testing "when the user is logged in"
     (do-it "should return an activity"
-      (with-environment :test
-        (let [user (model.user/create (factory User))]
-          (with-user user
-            (let [activity (factory Activity)
-                  response (create activity)]
-              (expect (activity? response))))))))
+      (let [user (model.user/create (factory User))]
+        (with-user user
+          (let [activity (factory Activity)
+                response (create activity)]
+            (expect (activity? response)))))))
   (testing "when the user is not logged in"
     (do-it "should return nil"
-      (with-environment :test
-        (let [activity (factory Activity)
-              response (create activity)]
-          (expect (nil? response)))))))
+      (let [activity (factory Activity)
+            response (create activity)]
+        (expect (nil? response))))))
 
 (describe index
   (testing "when there are no activities"
     (do-it "should be empty"
-      (with-environment :test
-        (drop!)
-        (let [response (index)]
-          (expect (empty? response))))))
+      (drop!)
+      (let [response (index)]
+        (expect (empty? response)))))
   (testing "when there are activities"
     (do-it "should return a seq of activities"
-      (with-environment :test
-        (let [actor (model.user/create (factory User))]
-          (drop!)
-          (with-user actor
-            (let [activity (create (factory Activity))
-                  response (index)]
-             (expect (seq response))
-             (expect (every? activity? response)))))))))
+      (let [actor (model.user/create (factory User))]
+        (drop!)
+        (with-user actor
+          (let [activity (create (factory Activity))
+                response (index)]
+            (expect (seq response))
+            (expect (every? activity? response))))))))
 
 (describe show
   (testing "when the record exists"
     (testing "and the user is not logged in"
       (testing "and the record is public"
         (do-it "should return the activity"
-          (with-environment :test
-            (let [author (model.user/create (factory User))
-                  activity (with-user author
-                             (create (factory Activity)))
-                  response (show (:_id activity))]
-              (expect (activity? response))))))
+          (let [author (model.user/create (factory User))
+                activity (with-user author
+                           (create (factory Activity)))
+                response (show (:_id activity))]
+            (expect (activity? response)))))
       (testing "and the record is not public"
         (do-it "should return nil"
-          (with-environment :test
-            (let [author (model.user/create (factory User))
-                  activity (with-user author
-                             (create (factory Activity {:public false})))
-                  response (show (:_id activity))]
-              (expect (nil? response)))))))
+          (let [author (model.user/create (factory User))
+                activity (with-user author
+                           (create (factory Activity {:public false})))
+                response (show (:_id activity))]
+            (expect (nil? response))))))
     (testing "and the user is logged in"
       (testing "and is the author"
         (do-it "should return the activity"
-          (with-environment :test
-            (let [user (model.user/create (factory User))]
-              (with-user user
-                (let [activity (create (factory Activity))
-                      response (show (:_id activity))]
-                  (expect (activity? response))))))))
+          (let [user (model.user/create (factory User))]
+            (with-user user
+              (let [activity (create (factory Activity))
+                    response (show (:_id activity))]
+                (expect (activity? response)))))))
       (testing "and is not the author"
         (testing "and is on the access list"
           (do-it "should return the activity"))
         (testing "and is not on the access list"
           (testing "and is an admin"
             (do-it "should return the activity"
-              (with-environment :test
-                (let [user (model.user/create (factory User {:admin true}))
-                      author (model.user/create (factory User))]
-                  (let [activity (with-user author
-                                   (create (factory Activity {:public false})))]
-                    (with-user user
-                      (let [response (show (:_id activity))]
-                        (expect (activity? response)))))))))
+              (let [user (model.user/create (factory User {:admin true}))
+                    author (model.user/create (factory User))]
+                (let [activity (with-user author
+                                 (create (factory Activity {:public false})))]
+                  (with-user user
+                    (let [response (show (:_id activity))]
+                      (expect (activity? response))))))))
           (testing "and is not an admin"
             (do-it "should return nil"
-              (with-environment :test
-                (let [user (model.user/create (factory User))
-                      author (model.user/create (factory User))]
-                  (let [activity
-                        (with-user author
-                          (create (factory Activity {:public false})))]
-                    (with-user user
-                      (let [response (show (:_id activity))]
-                        (expect (nil? response))))))))))))
+              (let [user (model.user/create (factory User))
+                    author (model.user/create (factory User))]
+                (let [activity
+                      (with-user author
+                        (create (factory Activity {:public false})))]
+                  (with-user user
+                    (let [response (show (:_id activity))]
+                      (expect (nil? response)))))))))))
     (testing "and the record is not public"
       (testing "and the user is not logged in"
         (do-it "should return nil"
-          (with-environment :test
-            (let [activity (create (factory Activity {:public false}))
-                  response (show (:_id activity))]
-              (expect (nil? response))))))
+          (let [activity (create (factory Activity {:public false}))
+                response (show (:_id activity))]
+            (expect (nil? response)))))
       (testing "and the user is logged in"
         (testing "and the user is an admin"
           (do-it "should return the activity")))))
@@ -147,18 +138,16 @@
 (describe drop!
   (testing "when there are activities"
     (do-it "should delete all of them"
-      (with-environment :test
-        (create (factory Activity))
-        (drop!)
-        (expect (empty? (index)))))))
+      (create (factory Activity))
+      (drop!)
+      (expect (empty? (index))))))
 
 (describe delete
   (testing "when a user is logged in"
     (testing "and is the owner of the activity"
       (do-it "should delete the activity"
-        (with-environment :test
-          (let [actor (model.user/create (factory User))]
-            (with-user actor
-              (let [activity (create (factory Activity))]
-                (delete (:_id activity))
-                (expect (nil? (show (:_id activity))))))))))))
+        (let [actor (model.user/create (factory User))]
+          (with-user actor
+            (let [activity (create (factory Activity))]
+              (delete (:_id activity))
+              (expect (nil? (show (:_id activity)))))))))))
