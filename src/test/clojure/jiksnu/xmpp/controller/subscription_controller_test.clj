ecb66d1c0e3739ac1242a4d10b6d8efa512b6231
Daniel E. Renfer <duck@kronkltd.net>
2011-03-05 13:26:32 -0500

    subscription tests

--- src/test/clojure/jiksnu/xmpp/controller/subscription_controller_test.clj
+++ src/test/clojure/jiksnu/xmpp/controller/subscription_controller_test.clj
@@ -3,8 +3,9 @@
         [jiksnu.mock :only (mock-subscriber-query-request-element
                             mock-subscription-query-request-element)]
         jiksnu.model
+        jiksnu.namespace
         jiksnu.xmpp.controller.subscription-controller
-        [jiksnu.xmpp.view :only (make-request make-jid make-packet)]
+        [jiksnu.xmpp.view :only (make-request make-jid make-packet make-element)]
         [lazytest.describe :only (describe do-it testing)]
         [lazytest.expect :only (expect)])
   (:require [jiksnu.model.subscription :as model.subscription]
@@ -12,35 +13,19 @@
   (:import jiksnu.model.Subscription
            jiksnu.model.User))
 
-(describe subscribers
-  (testing "when there are subscribers"
-    (do-it "should not be empty"
-      (let [user (model.user/create (factory User))
-            subscriber (model.user/create (factory User))
-            packet (make-packet
-                    {:to (make-jid user)
-                     :from (make-jid user)
-                     :type :get
-                     :body (mock-subscriber-query-request-element)})
-            request (make-request packet)
-            s (model.subscription/create
-               (factory Subscription
-                        {:from (:_id subscriber)
-                         :to (:_id user)}))
-            response (subscribers request)]
-        (expect (seq response))
-        (expect (every? (partial instance? Subscription) response))))))
-
-(describe subscriptions
+(describe subscriptions {:focus true}
   (testing "when there are subscriptions"
     (do-it "should return a sequence of subscriptions"
       (let [user (model.user/create (factory User))
             subscribee (model.user/create (factory User))
+            element (make-element
+                     "pubsub" {"xmlns" pubsub-uri}
+                     ["subscriptions" {"node" microblog-uri}])
             packet (make-packet
                     {:to (make-jid user)
                      :from (make-jid user)
                      :type :get
-                     :body (mock-subscription-query-request-element)})
+                     :body element})
             request (make-request packet)
             subscription (model.subscription/create
                           (factory Subscription
@@ -49,3 +34,35 @@
             results (subscriptions request)]
         (expect (not (empty? results)))
         (expect (every? (partial instance? Subscription) results))))))
+
+(describe subscribers {:focus true}
+  (testing "when there are subscribers"
+    (do-it "should not be empty"
+      (let [user (model.user/create (factory User))
+            subscriber (model.user/create (factory User))
+            element (make-element
+                     "pubsub" {"xmlns" pubsub-uri}
+                     ["subscribers" {"node" microblog-uri}])
+            packet (make-packet
+                    {:to (make-jid user)
+                     :from (make-jid user)
+                     :type :get
+                     :body element})
+            request (make-request packet)
+            subscription (model.subscription/create
+                          (factory Subscription
+                                   {:from (:_id subscriber)
+                                    :to (:_id user)}))
+            response (subscribers request)]
+        (expect (seq response))
+        (expect (every? (partial instance? Subscription) response))))))
+
+(describe subscribe)
+
+(describe remote-subscribe)
+
+(describe remote-subscribe-confirm)
+
+(describe subscribed)
+
+(describe unsubscribe)
