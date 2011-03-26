2480799af7c61c40d518f358df9416370fb0f18e
Daniel E. Renfer <duck@kronkltd.net>
2011-03-26 10:45:52 -0400

    view test

--- src/test/clojure/jiksnu/xmpp/view_test.clj
+++ src/test/clojure/jiksnu/xmpp/view_test.clj
@@ -22,17 +22,6 @@
 
 (describe node-value)
 
-(describe children)
-
-(describe ns-prefix
-  (testing "when the key name is empty"
-    (do-it "should have just the xmlns"
-      (let [k ""
-            response (ns-prefix k)]
-        (expect (= response "xmlns"))))))
-
-(describe element?)
-
 (describe pubsub-element?)
 
 (describe packet?)
@@ -92,17 +81,9 @@
 ;;                 response (to-tigase-element element)]
 ;;             (expect (element? response))))))))
 
-(describe assign-namespace)
-
-(describe element-name)
-
 (describe add-children)
 
 (describe add-attributes)
-
-(describe parse-qname)
-
-(describe merge-namespaces)
 
 (describe get-qname)
 
@@ -117,16 +98,6 @@
               response (abdera-to-tigase-element abdera-element)]
           (expect (element? response)))))))
 
-(describe make-element
-  (testing "with a complex structure"
-    (do-it "should return an element"
-      (let [response  (make-element
-                       "iq" {"type" "get"}
-                       ["pubsub" {"xmlns" pubsub-uri}
-                        ["items" {"node" microblog-uri}
-                         ["item" {"id" "test-id"}]]])]
-        (expect (element? response))))))
-
 (describe respond-with)
 
 (describe make-minimal-item)
@@ -134,16 +105,5 @@
 (describe apply-template)
 
 (describe make-jid)
-
-(describe make-packet
-  (do-it "should return a packet"
-    (let [user (model.user/create (factory User))
-          packet-map {:to (make-jid user)
-                      :from (make-jid user)
-                      :type :get
-                      :body (make-element
-                             ["pubsub" {}])}
-          response (make-packet packet-map)]
-      (expect (packet? response)))))
 
 (describe deliver-packet!)
