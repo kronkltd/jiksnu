1723dc4cd36ee70dfe3ff0a0f5c39960185605a3
Daniel E. Renfer <duck@kronkltd.net>
2011-04-01 12:22:59 -0400

    routes

--- src/main/clojure/jiksnu/routes.clj
+++ src/main/clojure/jiksnu/routes.clj
@@ -4,7 +4,8 @@
         clojure.contrib.logging
         hiccup.core
         jiksnu.http.middleware
-        [ring.middleware.session :only (wrap-session)])
+jiksnu.namespace
+                [ring.middleware.session :only (wrap-session)])
   (:require [ciste.middleware :as middleware]
             [compojure.core :as compojure]
             [jiksnu.http.view :as view]
@@ -23,7 +24,16 @@
              subscription-view
              user-view
              webfinger-view)
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            (jiksnu.xmpp.controller
+             [activity-controller :as activity]
+             [subscription-controller :as subscription]
+             [user-controller :as user])
+            [jiksnu.xmpp.view :as view]
+            compojure.core
+            clout.core)
+(:import tigase.xmpp.StanzaType)
+  )
 
 (def #^:dynamic *standard-middleware*
   [#'wrap-log-request
@@ -112,3 +122,143 @@
       (wrap-session)
       (middleware/wrap-http-serialization)
       (wrap-error-catching)))
+
+(dosync
+ (ref-set
+  *routes*
+  (map
+   (fn [[m a]]
+     [(merge {:serialization :xmpp
+              :format :xmpp} m) a])
+   [[{:method :get
+      :pubsub true
+      :name "items"
+      :node microblog-uri}
+     #'activity/index]
+
+    [{:method :set
+      :pubsub true
+      :name "publish"
+      :node microblog-uri}
+     #'activity/create]
+
+    [{:method :get
+      :pubsub true
+      :name "items"
+      :node (str microblog-uri ":replies:item=:id")}
+     #'activity/fetch-comments]
+
+    [{:method :get
+      :pubsub true
+      :node inbox-uri}
+     #'user/inbox]
+
+    [{:method :get
+      :name "query"
+      :ns query-uri}
+     #'user/show]
+
+    [{:method :set
+      :name "publish"
+      :ns vcard-uri}
+     #'user/create]
+
+    [{:method :result
+      :name "query"
+      :ns query-uri}
+     #'user/remote-create]
+
+    [{:method :get
+      :name "subscriptions"}
+     #'subscription/subscriptions]
+
+    [{:method :set
+      :name "subscribe"
+      :ns pubsub-uri}
+     #'subscription/subscribe]
+
+    [{:method :set
+      :name "unsubscribe"
+      :ns pubsub-uri}
+     #'subscription/unsubscribe]
+
+    [{:method :get
+      :name "subscribers"}
+     #'subscription/subscribers]
+
+    [{:method :set
+      :name "subscribers"}
+     #'subscription/subscribed]
+
+    [{:method :result
+      :name "subscription"
+      :ns pubsub-uri}
+     #'subscription/remote-subscribe-confirm]
+
+    [{:method :headline}
+     #'activity/remote-create]])))
+
+(defn node-matches?
+  [request matcher]
+  (if (:node matcher)
+    (if-let [response (clout.core/route-matches
+                       (clout.core/route-compile (:node matcher))
+                       (:node request))]
+      (assoc request :params response))
+    request))
+
+(defn type-matches?
+  [request matcher]
+  (if (:method matcher)
+    (if (= (:method matcher) (:method request))
+      request)
+    request))
+
+(defn name-matches?
+  [request matcher]
+  (if (:name matcher)
+    (if (= (:name matcher) (:name request))
+      request)
+    request))
+
+(defn ns-matches?
+  [request matcher]
+  (if (:ns matcher)
+        (if (= (:ns matcher) (:ns request))
+      request)
+    request))
+
+(defn http-serialization?
+  [request matcher]
+  (if (= (:serialization request) :http)
+    request))
+
+(defn xmpp-serialization?
+  [request matcher]
+  (if (= (:serialization request) :xmpp)
+    request))
+
+(defn request-method-matches?
+  [request matcher]
+  (if (#'compojure.core/method-matches (:method matcher) request)
+    request))
+
+(defn path-matches?
+  [request matcher]
+  (let [prepared (#'compojure.core/prepare-route (:path matcher))]
+    (if-let [route-params (#'clout.core/route-matches prepared request)]
+      (#'compojure.core/assoc-route-params request route-params))))
+
+(dosync
+ (ref-set *matchers*
+          [[#'xmpp-serialization?
+            [#'type-matches?
+             #'node-matches?
+             #'name-matches?
+             #'ns-matches?]]
+           [#'http-serialization?
+            [#'request-method-matches?
+             #'path-matches?]]]))
+
+(def #^:dynamic *standard-middleware*
+  [])
--- src/main/clojure/jiksnu/view.clj
+++ src/main/clojure/jiksnu/view.clj
@@ -1,11 +1,118 @@
 (ns jiksnu.view
   (:use ciste.core
+        ciste.html
         ciste.sections
         ciste.view
         [ciste.config :only (config)]
         clojure.contrib.logging
-        jiksnu.model)
+        hiccup.core
+        jiksnu.model
+        jiksnu.session)
+  (:require [hiccup.form-helpers :as f])
   (:import java.io.ByteArrayInputStream))
+
+(defn navigation-section
+  []
+  (let [user (current-user)]
+    [:nav
+    [:ul
+     [:li [:a {:href "/"} "Public"]]
+     (if user
+       (list
+        [:li [:a {:href (str (uri user) "/all")} "Home"]]
+        [:li [:a {:href "/settings/profile"} "Profile"]]
+        [:li [:a {:href "/domains"} "Domains"]]
+        (if (is-admin? user)
+          (list
+           [:li [:a {:href "/admin/users"} "Users"]]
+           [:li [:a {:href "/admin/subscriptions"} "Subscriptions"]])))
+       (list
+        [:li [:a {:href "/main/register"} "Register"]]))]]))
+
+(defn login-uri
+  []
+  "/main/login")
+
+(defn logout-uri
+  []
+  "/main/logout")
+
+(defn logout-form
+  []
+  [:div
+   (f/form-to
+    [:post (logout-uri)]
+    [:p "Logged in as:"
+     (link-to (current-user))
+     (f/submit-button "Logout")])])
+
+(defn login-section
+  []
+  [:div
+   (if-let [user (current-user)]
+     (logout-form)
+     [:a {:href "/main/login"} "Log in"])])
+
+(defn devel-environment-section
+  []
+  [:div.important.devel-section
+   "This site is running in development mode. No guarantees are made about the accuracy or security of information on this site. Use at your own risk."])
+
+(defn page-template-content
+  [response]
+  {:headers {"Content-Type" "text/html"}
+   :body
+   (str
+    "<!doctype html>\n"
+    (html
+     [:html
+      [:head
+       [:title "jiksnu"]
+       (map
+        (fn [link]
+          [:link {:type "application/atom+xml"
+                  :href link
+                  :rel "alternate"}])
+        (:links response))
+       #_(link-to-stylesheet
+          "/public/css/smoothness/jquery-ui-1.8.4.custom.css")
+       (link-to-stylesheet "/public/standard.css")]
+      [:body
+       [:div#wrap
+        [:header
+         (navigation-section)
+         (login-section)]
+        [:div#content
+         (devel-environment-section)
+         (:body response)]
+        [:footer
+         [:p "Copyright © 2011 KRONK Ltd."]]]
+       (link-to-script "http://code.jquery.com/jquery-1.4.4.min.js")
+       #_(link-to-script "/public/js/jquery-ui-1.8.4.custom.min.js")
+       (link-to-script "/public/standard.js")]]))})
+
+(defmethod serialize-as :http
+  [serialization response-map]
+  (merge {:headers {"Content-Type" "text/html"}}
+         response-map
+         (if-let [body (:body response-map)]
+           {:body (html body)})))
+
+(defmethod apply-template :html
+  [request response]
+  (merge
+   response
+   (if-not (= (:template response) false)
+     (page-template-content response))))
+
+(defsection link-to :default
+  [record & options]
+  (let [options-map (apply hash-map options)]
+    [:a
+     (apply merge {:href (uri record)} options-map)
+     [:span {:about (uri record)
+             :property "dc:title"}
+      (or (:title options-map) (title record))] ]))
 
 (defn parse-stream
   [stream]
