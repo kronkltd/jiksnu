2efe69dd949760bdc3cea144dc272c70f4caae42
Daniel E. Renfer <duck@kronkltd.net>
2011-04-04 18:50:50 -0400

    domains

--- src/main/clojure/jiksnu/filters/domain_filters.clj
+++ src/main/clojure/jiksnu/filters/domain_filters.clj
@@ -1,38 +1,39 @@
-(ns jiksnu.actions.domain-actions
-  (:use ciste.core
+(ns jiksnu.filters.domain-filters
+  (:use ciste.filters
+        jiksnu.actions.domain-actions
         jiksnu.actions.webfinger-actions)
   (:require [jiksnu.model.domain :as model.domain]))
 
-(defaction check-webfinger
-  [domain]
+(deffilter #'check-webfinger :http
+  [action domain]
   true)
 
-(defaction create
-  [request]
+(deffilter #'create :http
+  [action request]
   (let [{{domain "domain"} :params} request]
     (model.domain/create {:_id domain})))
 
-(defaction delete
-  [request]
+(deffilter #'delete :http
+  [action request]
   (let [{{id "*"} :params} request
         response (model.domain/delete id)]
     response))
 
-(defaction discover
-  [request]
+(deffilter #'discover :http
+  [action request]
   (let [{{id "id"} :params} request
         domain (model.domain/show id)
         xrd (fetch (str "http://" id "/.well-known/host-meta"))
         links (get-links xrd)]
     (model.domain/update (assoc domain :links links))))
 
-(defaction index
-  [request]
+(deffilter #'index :http
+  [action request]
   (let [domains (model.domain/index)]
     domains))
 
-(defaction show
-  [request]
+(deffilter #'show :http
+  [action request]
   (let [{{id "*"} :params} request
         domain (model.domain/show id)]
     domain))
