d86532537043fad131e869f07b26df537f6d1470
Daniel E. Renfer <duck@kronkltd.net>
2011-04-09 01:59:46 -0400

    clean up like's ns form

--- src/main/clojure/jiksnu/actions/like_actions.clj
+++ src/main/clojure/jiksnu/actions/like_actions.clj
@@ -1,18 +1,12 @@
 (ns jiksnu.actions.like-actions
   (:use jiksnu.model
-        jiksnu.session
-        )
-  (:require [jiksnu.model.like :as model.like]
-            )
-  (:import jiksnu.model.Like)
-  )
+        jiksnu.session)
+  (:require [jiksnu.model.like :as model.like])
+  (:import jiksnu.model.Like))
 
 (defn admin-index
   [request]
   {:body
    [:div
     
-    ]
-
-   }
-  )
+    ]})
