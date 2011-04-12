d5abb2865598694231f4cf0bf14791f77c21a2fc
Daniel E. Renfer <duck@kronkltd.net>
2011-04-11 23:57:16 -0400

    checkpoint

--- src/main/clojure/jiksnu/config.clj
+++ src/main/clojure/jiksnu/config.clj
@@ -12,30 +12,13 @@
                  :registration-enabled false
                  :debug false
                  :print {:request true
-                         :matchers true
-                         :predicates true
+                         :matchers false
+                         :predicates false
                          :params false}}
    :test {:domain "test.jiksnu.com"
           :registration-enabled false
           :database {:host "localhost"
                      :name :jiksnu_test}}}))
-
-;; (def #^:dynamic *environments*
-;;   (ref {:development {:database {:host "localhost"
-;;                                  :name :jiksnu_development}
-;;                       :domain "beta.jiksnu.com"
-;;                       :registration-enabled false
-;;                       :debug false
-;;                       :print {:request true
-;;                               :params false}}
-;;         :test {:domain "test.jiksnu.com"
-;;                :registration-enabled false
-;;                :database {:host "localhost"
-;;                           :name :jiksnu_test}}}))
-
-;; (defn config
-;;   []
-;;   (get @*environments* *current-environment*))
 
 (defmacro with-debug
   [& body]
--- src/main/clojure/jiksnu/xmpp/plugin.clj
+++ src/main/clojure/jiksnu/xmpp/plugin.clj
@@ -46,7 +46,7 @@
   [queue request]
   (let [merged-request (merge {:serialization :xmpp
                                :format :xmpp} request)
-        route-fn (resolve-routes *predicates* *routes*)]
+        route-fn (resolve-routes (lazier *predicates*) *routes*)]
     (route-fn merged-request)))
 
 (defn -process
