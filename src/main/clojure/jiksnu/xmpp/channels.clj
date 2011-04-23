be26f44043ddca66aed717218b3075d97b21e227
Daniel E. Renfer <duck@kronkltd.net>
2011-04-22 23:28:13 -0400

    channels work

---
+++ src/main/clojure/jiksnu/xmpp/channels.clj
@@ -0,0 +1,48 @@
+(ns jiksnu.xmpp.channels
+  (:use ciste.debug)
+  (:gen-class
+   :extends tigase.server.AbstractMessageReceiver
+   )
+  )
+
+(def packet-types-key "packet-types")
+(def prepend-text-key "log-prepend")
+(def secure-logging-key "secure-logging")
+
+(def packet-types
+  (into-array String ["message" "presence" "iq"]))
+(def prepend-text "Channel Server")
+(def secure-logging false)
+
+;; (defn -getDefaults
+;;   [this params]
+;;   (let [default-params  (proxy-super params)]
+;;     (println "getting defaults")
+;;     (.put default-params packet-types-key packet-types)
+;;     (.put default-params prepend-text-key prepend-text)
+;;     (.put default-params secure-logging-key secure-logging)
+;;     default-params)
+;;   )
+
+;; (defn -setProperties
+;;   [this props]
+;;   ;; (println "setting properties")
+;;   ;; (proxy-super props)
+;;   ;; (let [packet-types (.get props packet-types-key)]
+;;   ;;   (spy packet-types)
+;;   ;;   )
+;;   )
+
+(defn -processPacket
+  [this packet]
+  (println "")
+  (println "")
+  (println "")
+  (println "process packet")
+  (spy packet)
+  
+  (println "")
+  (println "")
+  (println "")
+
+  )
