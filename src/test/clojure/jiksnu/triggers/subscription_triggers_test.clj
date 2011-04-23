48fa9bf477e13b41b5800cddb8edcd463abed28f
Daniel E. Renfer <duck@kronkltd.net>
2011-04-23 12:06:00 -0400

    fix test

--- src/test/clojure/jiksnu/triggers/subscription_triggers_test.clj
+++ src/test/clojure/jiksnu/triggers/subscription_triggers_test.clj
@@ -5,11 +5,8 @@
         [lazytest.describe :only (describe testing do-it for-any)]
         [lazytest.expect :only (expect)])
   (:require [jiksnu.model.subscription :as model.subscription]
-            [jiksnu.model.user :as model.user]
-            
-            )
-  (:import jiksnu.model.User)
-  )
+            [jiksnu.model.user :as model.user])
+  (:import jiksnu.model.User))
 
 (describe notify-subscribe-xmpp
   (do-it "should return a packet"
@@ -17,7 +14,7 @@
           subscribee (model.user/create (factory User))
           subscription (model.subscription/subscribe
                         (:_id user) (:_id subscribee))
-          response (notify-subscribe {:id "JIKSNU1"} subscription)]
+          response (notify-subscribe-xmpp {:id "JIKSNU1"} subscription)]
       (expect (packet? response)))))
 
 (describe notify-unsubscribe-xmpp)
