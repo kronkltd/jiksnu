63b2407780c895d3dd231314dc4b115a8a4418cb
Daniel E. Renfer <duck@kronkltd.net>
2011-08-11 07:47:37 -0400

    set mongo dbs

--- src/main/clojure/jiksnu/core.clj
+++ src/main/clojure/jiksnu/core.clj
@@ -1,13 +1,17 @@
 (ns jiksnu.core
   (:use [ciste.config :only (load-config)])
-  (:require [jiksnu.http :as http]
-            jiksnu.routes
-            [jiksnu.xmpp :as xmpp]))
+  (:require (jiksnu [http :as http]
+                    [model :as model]
+                    routes
+                    [xmpp :as xmpp])))
 
 (defn start
   ([] (start 8082))
   ([port]
      (load-config)
+     (dosync
+      (ref-set model/*mongo-database*
+               (mongo-database*)))
      (http/start port)
      (xmpp/start)))
 
