94fed6501071956a6e4cd15b521499c39d010104
Daniel E. Renfer <duck@kronkltd.net>
2011-08-12 12:28:03 -0400

    features

--- features/step_definitions/page_steps.clj
+++ features/step_definitions/page_steps.clj
@@ -1,41 +1,57 @@
 (use 'jiksnu.http)
 (use 'jiksnu.model)
 (use 'clj-factory.core)
+(use 'ring.mock.request)
 (import 'jiksnu.model.Activity)
 (require 'jiksnu.model.activity)
 (require 'ciste.config)
+(use 'aleph.http)
+(use 'aleph.formats)
+(use 'clojure.test)
 
 (def server (atom nil))
+(def current-page (ref nil))
 
 (Before
- (alter-var-root #'ciste.config/*environment* :test)
- (dosync
-  (ref-set jiksnu.model/*mongo-database* (mongo-database))
-  (reset! server (with-environment :test (start 8085)))))
+  (dosync
+   (with-environment :test
+     (ciste.config/load-config)
+     (println "Starting server")
+     (reset! server (with-environment :test (start 8085))))))
 
 (After
- (println "Going down")
- (println server)
- (@server)
- )
+  (println "Going down")
+  (println @server)
+  (@server)
+  (println "shutting down agents")
+  (shutdown-agents)
+  (println "after is done"))
+
 
 (Given #"the user is not logged in"
-      #(identity nil)
-
-       )
+  (fn []
+    (Thread/sleep 3000)
+    (println "done sleeping")))
 
 (Given #"an? activity exists"
-       (fn []
-         (with-environment :test
-          (jiksnu.model.activity/create (factory Activity)))))
+  (fn []
+    (with-environment :test
+      (println (jiksnu.model.activity/create (factory Activity))))))
 
 (When #"I visit the home page"
-      #(identity nil)
-      )
+  (fn []
+    (let [request {:method :get
+                   :url "http://localhost:8085/"}
+          response (sync-http-request request)]
+      (dosync
+       (ref-set current-page response)))))
 
 (Then #"I should see an activity"
-      #(println "seeing an activity"))
+  (fn []
+    (let [response @current-page
+          body (channel-buffer->string (:body response))]
+      (is (re-matches #".*hentry.*" body)))))
 
 (Then #"I should see a list of activities"
-      #(identity nil)
-      )
+  (fn []
+    (identity nil)))
