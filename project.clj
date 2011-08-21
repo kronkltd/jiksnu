595b3e2bf73ec3d962c26117dcf1b64adc2ca2b2
Daniel E. Renfer <duck@kronkltd.net>
2011-08-21 02:06:57 -0400

    switch to leiningen
    fixes #2

---
+++ project.clj
@@ -0,0 +1,44 @@
+(defproject net.kronkltd/jiksnu "0.1.0-SNAPSHOT"
+  :description "a framework for asynchronous communication"
+  :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
+                 "jiksnu-internal" "http://build.jiksnu.com/repository/internal"
+                 "jiksnu-snapshots" "http://build.jiksnu.com/repository/snapshots"
+                 }
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"]
+                 [org.clojure/tools.logging "0.1.2"]
+                 [noir "1.1.1-SNAPSHOT"]
+                 [noir-cljs "0.1.0-SNAPSHOT"]
+                 [net.kronkltd/lamina "0.4.0-beta2-SNAPSHOT"]
+                 [ring/ring-core "0.3.11"]
+                 [ring-mock "0.1.1"]
+                 [net.kronkltd/ciste "0.1.0-SNAPSHOT"]
+                 [compojure "0.6.5"]
+                 [net.kronkltd/clj-factory "0.1.0-SNAPSHOT"]
+                 [hiccup "0.3.6"]
+                 [net.kronkltd/karras "0.6.0"]
+                 [closure-templates-clj "0.0.1"]
+                 [net.kronkltd/clj-tigase "0.1.0-SNAPSHOT"]
+                 [org.apache.abdera/abdera-core "1.1.1"]
+                 [org.apache.abdera/abdera-parser "1.1.1"]
+                 [org.apache.abdera/abdera-extensions-json "1.1.1"]
+                 [org.apache.ws.commons.axiom/axiom-impl "1.2.9"]
+                 [net.kronkltd/clj-gravatar "0.0.1"]
+                 [com.cliqset/java-salmon-core "0.0.1-SNAPSHOT"]
+                 [com.cliqset/java-salmon-simple "0.0.1-SNAPSHOT"]
+                 [com.cliqset.abdera/abdera-extensions-activitystreams "0.1.0-SNAPSHOT"]
+                 [mysql/mysql-connector-java "5.1.15"]
+                 [com.ocpsoft/ocpsoft-pretty-time "1.0.6"]
+                 [net.kronkltd/plaza-core "0.0.6-SNAPSHOT"]
+                 [net.kronkltd/aleph "0.2.0-beta2-SNAPSHOT"]
+                 [org.deri.any23/any23-core "0.5.0"]
+                 [org.slf4j/slf4j-simple "1.6.1"]]
+  :exclusions [org.clojure/contrib
+               org.slf4j/slf4j-log4j12
+               org.slf4j/slf4j-jdk14
+               ]
+  :aot [jiksnu.xmpp.plugin]
+  :warn-on-reflection false
+  :jvm-opts ["-server"])
