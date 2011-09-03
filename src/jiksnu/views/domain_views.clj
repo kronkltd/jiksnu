a86871a13ed36d3d804d57ca839ad45440d83ac8
Daniel E. Renfer <duck@kronkltd.net>
2011-09-03 13:25:05 -0400

    spies

--- src/jiksnu/views/domain_views.clj
+++ src/jiksnu/views/domain_views.clj
@@ -10,7 +10,7 @@
 (defn ping-request
   [domain]
   {:type :get
-   :to (tigase/make-jid "" (:_id (spy domain)))
+   :to (tigase/make-jid "" (:_id domain))
    :from (tigase/make-jid "" (config :domain))
    :body (element/make-element ["ping" {"xmlns" "urn:xmpp:ping"}])})
 
