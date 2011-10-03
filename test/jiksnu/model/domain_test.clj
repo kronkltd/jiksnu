575517d25552f8e1a11638efdbb843c3ef90007d
Daniel E. Renfer <duck@kronkltd.net>
2011-10-03 02:36:24 -0400

    domain test

--- test/jiksnu/model/domain_test.clj
+++ test/jiksnu/model/domain_test.clj
@@ -11,7 +11,33 @@
 
 (use-fixtures :once test-environment-fixture)
 
+(deftest test-drop!)
+
+(deftest test-show!)
+
+(deftest test-index)
+
+(deftest test-create)
+
+(deftest test-update)
+
+(deftest test-delete)
+
+(deftest test-find-or-create)
+
+(deftest test-add-links)
+
+(deftest test-set-field)
+
+(deftest test-set-discovered)
+
 (deftest test-ping-request
   (fact "should return a ping packet"
     (let [domain (actions.domain/create (factory Domain))]
       (ping-request domain) => (contains {:body element?}))))
+
+(deftest test-pending-domains-key
+  (fact "should return a key name"
+    (let [domain (actions.domain/create (factory Domain))]
+      (pending-domains-key domain) => string?)))
+
