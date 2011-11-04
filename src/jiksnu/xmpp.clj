cdafe1bc97269b7177c58523754a2ee7ebd6d042
Daniel E. Renfer <duck@kronkltd.net>
2011-11-04 07:24:27 -0400

    use config file to generate tigase config

--- src/jiksnu/xmpp.clj
+++ src/jiksnu/xmpp.clj
@@ -1,27 +1,38 @@
 (ns jiksnu.xmpp
-  (:use clj-tigase.core ))
+  (:use (ciste [config :only [config definitializer]]
+               [debug :only [spy]])
+        clj-tigase.core)
+  (:require (clojure [string :as string])))
 
 (def ^:dynamic *initial-config*
-     (str
+    "" #_(str
       "tigase.level=ALL\n"
       "tigase.xml.level=INFO\n"
       "handlers=java.util.logging.ConsoleHandler\n"
       "java.util.logging.ConsoleHandler.level=ALL\n"
       "java.util.logging.ConsoleHandler.formatter=tigase.util.LogFormatter\n"))
 
-(def tigase-options
-     (into-array
+(defn tigase-options
+  []
+  (into-array
       String
-      ["-c"
-       "etc/tigase.xml"
-       "--property-file"
-       "etc/init.properties"]))
+      ["--admins" (->> (config :admins)
+                       (map (fn [username]
+                              ;; TODO: ensure user created
+                              (str username "@" (config :domain))))
+                       (string/join "," ))
+       "--auth-db" "jiksnu.xmpp.user_repository"
+       "--user-db" "jiksnu.xmpp.user_repository"
+       "--debug" "server"
+       "--sm-plugins" "jiksnu,message"
+       "--c2s-ports" (str (config :xmpp :c2s))
+       "--s2s" (str (config :xmpp :s2s))
+       "--virt-hosts" (config :domain)
+       "--comp-name-1" "channels"
+       "--comp-class-1" "jiksnu.xmpp.channels"]))
 
 (defn start
   []
-  (let [config (get-config *initial-config* tigase-options)]
-    (start-router! tigase-options config)))
-
-(defn -main
-  []
-  (start))
+  (let [tigase-config (get-config *initial-config* (tigase-options))]
+    (spy (bean tigase-config))
+    (start-router! tigase-options tigase-config)))
