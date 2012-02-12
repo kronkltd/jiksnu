ce52c27c087d90647b346b747d6b34f5a885d5da
Daniel E. Renfer <duck@kronkltd.net>
2012-02-11 22:19:57 -0500

    subscription helpers

--- src/jiksnu/helpers/subscription_helpers.clj
+++ src/jiksnu/helpers/subscription_helpers.clj
@@ -1,17 +1,14 @@
 (ns jiksnu.helpers.subscription-helpers
-  (:use (ciste core
-               [debug :only (spy)]
-               sections)
+  (:use (ciste [debug :only [spy]])
         ciste.sections.default
-        clj-tigase.core
-        (jiksnu model view))
-  (:require (jiksnu [namespace :as namespace])
+        clj-tigase.core)
+  (:require (jiksnu [namespace :as ns])
             (jiksnu.model [user :as model.user])))
 
 (defn subscriber-response-element
   [subscription]
   (let [subscriber (model.user/fetch-by-id (:from subscription))]
-    ["subscriber" {"node" namespace/microblog
+    ["subscriber" {"node" ns/microblog
                    "created" (format-date (:created subscription))
                    "jid" (str (:username subscriber) "@"
                               (:domain subscriber))}]))
@@ -20,7 +17,7 @@
   [subscription]
   (let [subscribee (model.user/fetch-by-id (:to subscription))
         created (:created subscription)]
-    ["subscription" {"node" namespace/microblog
+    ["subscription" {"node" ns/microblog
                      "subscription" "subscribed"
                      "created" (format-date created)
                      "jid" (str (:username subscribee) "@"
@@ -29,26 +26,26 @@
 (defn unsubscription-request
   [subscription]
   (let [subscribee (model.user/fetch-by-id (:from subscription))]
-    ["pubsub"  {"xmlns" namespace/pubsub}
-     ["unsubscribe" {"node" namespace/microblog
+    ["pubsub"  {"xmlns" ns/pubsub}
+     ["unsubscribe" {"node" ns/microblog
                      "jid" (make-jid subscribee)}]]))
 
 (defn subscribe-request
   [subscription]
   (let [subscribee (model.user/fetch-by-id (:from subscription))]
-    ["pubsub"  {"xmlns" namespace/pubsub}
-     ["subscribe" {"node" namespace/microblog
+    ["pubsub"  {"xmlns" ns/pubsub}
+     ["subscribe" {"node" ns/microblog
                    "jid" (make-jid subscribee)}]]))
 
 (defn subscribers-response
   [subscribers]
-  ["pubsub" {"xmlns" namespace/pubsub}
-   ["subscribers" {"node" namespace/microblog}
+  ["pubsub" {"xmlns" ns/pubsub}
+   ["subscribers" {"node" ns/microblog}
     (map subscriber-response-element subscribers)]])
 
 (defn subscriptions-response
   "Returns a response iq packet containing the ids in entries"
   [subscriptions]
-  ["pubsub" {"xmlns" namespace/pubsub}
-   ["subscriptions" {"node" namespace/microblog}
+  ["pubsub" {"xmlns" ns/pubsub}
+   ["subscriptions" {"node" ns/microblog}
     (map subscription-response-element subscriptions)]])
