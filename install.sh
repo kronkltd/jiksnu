c538b80bbdb5a76662f84a702e06e9f0d498c784
Daniel E. Renfer <duck@kronkltd.net>
2012-03-07 11:47:31 -0500

    vagrant file

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y byobu
+sudo apt-get install -y emacs
+sudo apt-get install -y git-core
+sudo apt-get install -y leiningen
+sudo apt-get install -y rlwrap
+sudo apt-get install -y openjdk-6-jdk 
