b29a73281242cf9b6244e29f52e28272e515019f
Daniel E. Renfer <duck@kronkltd.net>
2012-03-06 23:04:41 -0500

    get links without content type

--- src/jiksnu/helpers/user_helpers.clj
+++ src/jiksnu/helpers/user_helpers.clj
@@ -24,7 +24,8 @@
 
 (defn feed-link-uri
   [^User user]
-  (if-let [link (model.user/get-link user namespace/updates-from "application/atom+xml")]
+  (if-let [link (or (model.user/get-link user namespace/updates-from "application/atom+xml")
+                    (model.user/get-link user namespace/updates-from nil))]
     (:href link)))
 
 (defn fetch-user-feed
