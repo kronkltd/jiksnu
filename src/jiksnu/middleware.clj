07bcbbc82be11d6b73ef098916082aff1c95848f
Daniel E. Renfer <duck@kronkltd.net>
2012-03-20 22:47:36 -0400

    remove new relic

--- src/jiksnu/middleware.clj
+++ src/jiksnu/middleware.clj
@@ -5,8 +5,7 @@
         (jiksnu [session :only [with-user-id]]))
   (:require (clojure [stacktrace :as st])
             (clojure.tools [logging :as log]))
-  (:import com.newrelic.api.agent.NewRelic
-           javax.security.auth.login.LoginException))
+  (:import javax.security.auth.login.LoginException))
 
 (defn wrap-user-binding
   [handler]
@@ -24,18 +23,3 @@
          :template false
          :flash "You must be logged in to do that."
          :headers {"location" "/main/login"}}))))
-
-(defn newrelic-report
-  [handler]
-  (fn [request]
-    (try
-      ;; (NewRelic/setTransactionName "requests" (:uri request))
-      ;; (with-time
-      ;;   (fn [m]
-      ;;     (NewRelic/recordMetric
-      ;;      (:uri request) (float (:elapsed m))))
-      (handler request)
-      ;; )
-      (catch Exception ex
-        (NewRelic/noticeError ex)
-        (throw ex)))))
