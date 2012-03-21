7e205428c8ccc64c0d9c3378d1ec650bed884aba
Daniel E. Renfer <duck@kronkltd.net>
2012-03-21 01:31:26 -0400

    package google closure library

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
-sudo apt-get update
-sudo apt-get install -y byobu
-sudo apt-get install -y emacs
-sudo apt-get install -y git
-sudo apt-get install -y leiningen
-sudo apt-get install -y rlwrap
-sudo apt-get install -y openjdk-6-jdk 
-sudo apt-get install -y maven2
+# sudo apt-get update
+# sudo apt-get install -y byobu
+# sudo apt-get install -y emacs
+# sudo apt-get install -y git
+# sudo apt-get install -y leiningen
+# sudo apt-get install -y rlwrap
+# sudo apt-get install -y openjdk-6-jdk 
+# sudo apt-get install -y maven2
 
+rm -rf jiksnu
+
+cp -r /vagrant jiksnu
+cd jiksnu
+mvn install:install-file -DgroupId=org.clojure -DartifactId=google-closure-library -Dversion=0.0-1589 -Dpackaging=jar -Dfile=google-closure-library-0.0-1589.jar
+lein deps
+lein run
