4d13fecc81990dca4857b7f48f1a5c3af525888e
Daniel E. Renfer <duck@kronkltd.net>
2012-04-12 22:06:04 -0400

    fork me on github

--- src/jiksnu/sections/layout_sections.clj
+++ src/jiksnu/sections/layout_sections.clj
@@ -184,6 +184,11 @@
           ;;   {:type "text" :placeholder "Search" :name "q"}]]
           [:ul.nav.pull-right (sections.auth/login-section response)]]]]
        [:div.container
+        [:a {:href "http://github.com/duck1123/jiksnu"}
+         [:img {:style "position: absolute; top: 43px; right: 0; border: 0;"
+                :src "https://a248.e.akamai.net/assets.github.com/img/7afbc8b248c68eb468279e8c17986ad46549fb71/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+                :alt "Fork me on GitHub"}]
+         ]
         [:div.row
          [:div.span2
           #_[:span#interface]
