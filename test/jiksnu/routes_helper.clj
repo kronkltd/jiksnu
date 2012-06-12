186dead1abca4bdc1cd47559fd445db948ac2032
Daniel E. Renfer <duck@kronkltd.net>
2012-06-12 07:49:56 -0400

    extract get-auth-cookie

--- test/jiksnu/routes_helper.clj
+++ test/jiksnu/routes_helper.clj
@@ -1,6 +1,11 @@
 (ns jiksnu.routes-helper
-  (:use [lamina.core :only [channel wait-for-message]])
-  (:require [jiksnu.routes :as r]))
+  (:use [clojure.core.incubator :only [-?> -?>>]]
+        [lamina.core :only [channel wait-for-message]])
+  (:require [clj-http.cookies :as cookies]
+            [clojure.tools.logging :as log]
+            [jiksnu.routes :as r]
+            [ring.mock.request :as mock]
+            [ring.util.codec :as codec]))
 
 (defn response-for
   "Run a request against the main handler and wait for the response"
@@ -10,3 +15,15 @@
        (r/app ch request)
        (wait-for-message ch timeout))))
 
+(defn get-auth-cookie
+  [username password]
+  (-?> (mock/request :post "/main/login")
+       (assoc :params {:username username
+                       :password password})
+       response-for
+       :headers
+       (get "Set-Cookie")
+       cookies/decode-cookies
+       (->> (map (fn [[k v]] [k (:value v)]))
+            (into {}))
+       codec/form-encode))
