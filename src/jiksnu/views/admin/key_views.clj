d6034922475ae6468dd7b050874c72a038b271a3
Daniel E. Renfer <duck@kronkltd.net>
2012-06-13 23:12:56 -0400

    basic key view

--- src/jiksnu/views/admin/key_views.clj
+++ src/jiksnu/views/admin/key_views.clj
@@ -1,2 +1,9 @@
-(ns jiksnu.views.admin.key-views)
+(ns jiksnu.views.admin.key-views
+  (:use [ciste.sections.default :only [index-section]]
+        [ciste.views :only [defview]]
+        [jiksnu.actions.admin.key-actions :only [index]]))
 
+(defview #'index :html
+  [request response]
+  {:body "foo"
+   :title "Keys"})
