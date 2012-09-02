fe98fef4a3f5809f5318e3c1b84ed8daa50637af
Daniel E. Renfer <duck@kronkltd.net>
2012-09-02 13:25:16 -0400

    checkpoint

--- test/jiksnu/filters/activity_filters_test.clj
+++ test/jiksnu/filters/activity_filters_test.clj
@@ -7,7 +7,7 @@
         [jiksnu.model :only [activity?]]
         [jiksnu.routes :only [app]]
         [jiksnu.session :only [with-user]]
-        jiksnu.xmpp.element
+        ;; jiksnu.xmpp.element
         midje.sweet)
   (:require [clj-tigase.core :as tigase]
             [clj-tigase.element :as element]
@@ -16,56 +16,53 @@
             [jiksnu.namespace :as ns]
             [jiksnu.actions.activity-actions :as actions.activity]
             [jiksnu.actions.user-actions :as actions.user]
-            jiksnu.filters.activity-filters
             [jiksnu.model.activity :as model.activity]
-            [jiksnu.model.user :as model.user]
-            [ring.mock.request :as mock])
-  (:import jiksnu.model.Activity
-           jiksnu.model.User))
-
+            [jiksnu.model.user :as model.user]))
 
 (test-environment-fixture
 
- (fact "filter-action #'actions.activity/create :xmpp"
+ (fact "filter-action #'actions.activity/create"
    (let [action  #'actions.activity/create]
-     (fact "when the user is logged in"
-       (fact "and it is a valid activity"
-         (fact "should return that activity"
-           (with-serialization :xmpp
-             (with-format :xmpp
+     (fact "when the serialization is :xmpp"
+       (with-serialization :xmpp
+         (fact "when the format is :xmpp"
+           (with-format :xmpp
+             (fact "when the user is logged in"
                (let [user (model.user/create (factory :local-user))]
                  (with-user user
-                   (let [activity (factory :activity)
-                         element (element/make-element
-                                  (index-section [activity]))
-                         packet (tigase/make-packet
-                                 {:to (tigase/make-jid user)
-                                  :from (tigase/make-jid user)
-                                  :type :set
-                                  :body element})
-                         request (packet/make-request packet)]
-                     (filter-action action request) => activity?))))))))))
+                   (fact "and it is a valid activity"
+                     (let [activity (factory :activity)
+                           element (element/make-element
+                                    (index-section [activity]))
+                           packet (tigase/make-packet
+                                   {:to (tigase/make-jid user)
+                                    :from (tigase/make-jid user)
+                                    :type :set
+                                    :body element})
+                           request (packet/make-request packet)]
+                       (filter-action action request) => activity?)))))))))))
 
- (fact "filter-action #'actions.activity/show :xmpp"
-   (let [action #'actions.activity/show ]
-     (with-serialization :xmpp
-       (let [author (model.user/create (factory :user))]
-         (with-user author
-           (let [activity (model.activity/create (factory :activity))
-                 packet-map {:from (tigase/make-jid author)
-                             :to (tigase/make-jid author)
-                             :type :get
-                             :id "JIKSNU1"
-                             :body (element/make-element
-                                    ["pubsub" {"xmlns" ns/pubsub}
-                                     ["items" {"node" ns/microblog}
-                                      ["item" {"id" (str (:_id activity))}]]])}
-                 packet (tigase/make-packet packet-map)
-                 request (packet/make-request packet)]
-             (filter-action action request) =>
-             (every-checker
-              activity?)))))))
-
+ (fact "filter-action #'actions.activity/show"
+   (let [action #'actions.activity/show]
+     (fact "when the serialization is :xmpp"
+       (with-serialization :xmpp
+         (let [author (model.user/create (factory :user))]
+           (with-user author
+             (let [activity (model.activity/create (factory :activity))
+                   packet-map {:from (tigase/make-jid author)
+                               :to (tigase/make-jid author)
+                               :type :get
+                               :id "JIKSNU1"
+                               :body (element/make-element
+                                      ["pubsub" {"xmlns" ns/pubsub}
+                                       ["items" {"node" ns/microblog}
+                                        ["item" {"id" (str (:_id activity))}]]])}
+                   packet (tigase/make-packet packet-map)
+                   request (packet/make-request packet)]
+               (filter-action action request) =>
+               (every-checker
+                activity?))))))))
+ 
  (fact "filter-action #'actions.activity/oembed"
    (let [action #'actions.activity/oembed]
      (fact "when the serialization is :http"
