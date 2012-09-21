78bab011b40fec0ce7b9db68bd0fba4f6f165f4b
Daniel E. Renfer <duck@kronkltd.net>
2012-09-20 21:42:18 -0400

    user routes

--- src/jiksnu/routes/user_routes.clj
+++ src/jiksnu/routes/user_routes.clj
@@ -8,25 +8,29 @@
 (add-route! "/users/:id/discover" {:named "discover user"})
 (add-route! "/users/:id/update"   {:named "update user"})
 (add-route! "/main/profile"       {:named "user profile"})
+(add-route! "/main/xrd"           {:named "user meta"})
+(add-route! "/model/users/:id"    {:named "user model"})
 
 (defn routes
   []
-  [[[:get    "/api/friendships/exists.:format"] #'user/exists?]
+  [
+   [[:get    (named-path     "register page")] #'user/register-page]
+   [[:post   (named-path     "register page")] #'user/register]
+   [[:get    (named-path     "user profile")]  #'user/profile]
+   [[:post   (named-path     "user profile")]  #'user/update-profile]
+   [[:get    (formatted-path "index users")]   #'user/index]
+   [[:get    (named-path     "index users")]   #'user/index]
+   [[:post   (formatted-path "discover user")] #'user/discover]
+   [[:post   (named-path     "discover user")] #'user/discover]
+   [[:post   (formatted-path "update user")]   #'user/fetch-updates]
+   [[:post   (named-path     "update user")]   #'user/fetch-updates]
+   [[:get    (named-path     "user meta")]     #'user/user-meta]
+   [[:get    (formatted-path "user model")]    #'user/show]
+
+   [[:get    "/api/friendships/exists.:format"] #'user/exists?]
    [[:get    "/api/people/@me/@all"]            #'user/index]
    [[:get    "/api/people/@me/@all/:id"]        #'user/show]
-   [[:get    (named-path "register page")]      #'user/register-page]
-   [[:post   "/main/register"]                  #'user/register]
-   [[:get    "/main/xrd"]                       #'user/user-meta]
-   [[:get    (named-path "user profile")]       #'user/profile]
-   [[:post   "/main/profile"]                   #'user/update-profile]
-   [[:get    "/users.:format"]                  #'user/index]
-   [[:get    (named-path "index users")]        #'user/index]
    [[:delete "/users/:id"]                      #'user/delete]
    [[:post   "/users/:id/delete"]               #'user/delete]
-   [[:post   "/users/:id/discover.:format"]     #'user/discover]
-   [[:post   (named-path "discover user")]      #'user/discover]
-   [[:post   "/users/:id/update.:format"]       #'user/fetch-updates]
-   [[:post   (named-path "update user")]        #'user/fetch-updates]
    ;; [[:post   "/users/:id/update-hub"]           #'user/update-hub]
-   [[:post   "/:username"]                      #'user/update]
-   [[:get "/model/users/:id.:format"]           #'user/show]])
+   [[:post   "/:username"]                      #'user/update]])
