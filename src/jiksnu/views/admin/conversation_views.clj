13a41b8c6f8460874f9ac1c3f3b5628e6484d29f
Daniel E. Renfer <duck@kronkltd.net>
2012-09-26 22:45:39 -0400

    conversation viewmodel

--- src/jiksnu/views/admin/conversation_views.clj
+++ src/jiksnu/views/admin/conversation_views.clj
@@ -2,7 +2,7 @@
   (:use [ciste.sections.default :only [show-section]]
         [ciste.views :only [defview]]
         [jiksnu.actions.admin.conversation-actions :only [index show]]
-        [jiksnu.sections :only [admin-index-section]])
+        [jiksnu.sections :only [admin-index-section format-page-info]])
   (:require [jiksnu.model.conversation :as model.conversation]))
 
 (defview #'index :html
@@ -10,6 +10,17 @@
   {:title "Conversations"
    :single true
    :body (admin-index-section items response)})
+
+(defview #'index :viewmodel
+  [request {:keys [items] :as page}]
+  {:body {:title "Conversations"
+          :pageInfo (format-page-info page)
+          :items (map :_id items)
+          :users (admin-index-section items page)}})
+
+
+
+
 
 (defview #'show :html
   [request conversation]
