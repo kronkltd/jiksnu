a7cd74327975662afa03556b51c8d2bef6eb2cc9
Daniel E. Renfer <duck@kronkltd.net>
2012-10-16 00:21:06 -0400

    count records test

--- test/jiksnu/model/conversation_test.clj
+++ test/jiksnu/model/conversation_test.clj
@@ -6,14 +6,17 @@
         [midje.sweet :only [fact future-fact =>]]
         [validateur.validation :only [valid?]])
   (:require [clojure.tools.logging :as log]
+            [jiksnu.actions.conversation-actions :as actions.conversation]
             [jiksnu.actions.user-actions :as actions.user]
             [jiksnu.features-helper :as feature]
-            [jiksnu.model :as model]
-            )
-  )
+            [jiksnu.model :as model]))
 
 (test-environment-fixture
  (fact "#'count-records"
-   (count-records) => 0
-   )
- )
+   (fact "when there aren't any conversations"
+     (count-records) => 0)
+   (fact "when there are conversations"
+     (let [n 15]
+       (dotimes [i n]
+         (actions.conversation/create (factory :conversation)))
+       (count-records) => n))))
