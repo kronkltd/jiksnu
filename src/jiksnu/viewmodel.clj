0b70c78be367e55eb6f1e0534cbf9c51f3c63c93
Daniel E. Renfer <duck@kronkltd.net>
2012-10-19 21:12:29 -0400

    viewmodel

---
+++ src/jiksnu/viewmodel.clj
@@ -0,0 +1,42 @@
+(ns jiksnu.viewmodel
+  (:use [ciste.commands :only [add-command!]]
+        [ciste.core :only [defaction]]
+        
+        [jiksnu.routes :only [http-predicates http-routes]]
+        [jiksnu.routes.admin-routes :only [admin-routes]]
+        [ciste.filters :only [deffilter filter-action]]
+        [ciste.routes :only [resolve-routes]]
+        [ciste.views :only [defview]]
+        [clojure.data.json :only [read-json]]))
+
+(defaction fetch-viewmodel
+  [path options]
+  (let [request {:request-method :get
+                 :uri            (str "/" path)
+                 :params         options
+                 :serialization  :http
+                 :format         :viewmodel}
+        response ((resolve-routes [http-predicates]
+                                  (concat http-routes
+                                          admin-routes))
+                  request)]
+    (if-let [body (:body response)]
+      (let [vm (read-json body)]
+        {:path path
+         :options options
+         :body {:action "update viewmodel"
+                :body vm}})
+      {:body {:action "error"
+              :body "could not find viewmodel"}})))
+
+(deffilter #'fetch-viewmodel :command
+  [action request]
+  (let [[path opt-string] (:args request)]
+    (action (read-json path) (read-json opt-string))))
+
+(defview #'fetch-viewmodel :json
+  [request response]
+  response)
+
+(add-command! "fetch-viewmodel" #'fetch-viewmodel)
+
