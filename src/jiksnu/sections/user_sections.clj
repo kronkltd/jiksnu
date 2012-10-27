2a5753d4145d6879ee6f69cd562057de9bd086f9
Daniel E. Renfer <duck@kronkltd.net>
2012-10-27 14:17:34 -0400

    user title

--- src/jiksnu/sections/user_sections.clj
+++ src/jiksnu/sections/user_sections.clj
@@ -504,7 +504,7 @@
   [record & options]
   (let [options-map (apply hash-map options)]
     [:a (if *dynamic*
-          {:data-bind "attr: {href: '/users/' + ko.utils.unwrapObservable(_id)}"}
+          {:data-bind "attr: {href: '/users/' + ko.utils.unwrapObservable(_id), title: 'acct:' + ko.utils.unwrapObservable(username) + '@' + ko.utils.unwrapObservable(domain)}"}
           {:href (uri record)})
      [:span (merge {:property "dc:title"}
                    (if *dynamic*
