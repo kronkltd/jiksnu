1b8f2bc424a73b2c0e25b6c14755f7fd8773ead1
Daniel E. Renfer <duck@kronkltd.net>
2012-10-28 14:24:11 -0400

    implement feed source view of activities

--- src/jiksnu/views/feed_source_views.clj
+++ src/jiksnu/views/feed_source_views.clj
@@ -4,12 +4,15 @@
         ciste.sections.default
         [clojurewerkz.route-one.core :only [named-path]]
         jiksnu.actions.feed-source-actions
-        [jiksnu.ko :only [*dynamic*]])
+        [jiksnu.ko :only [*dynamic*]]
+        [jiksnu.sections :only [format-page-info pagination-links with-page]])
   (:require [clojure.tools.logging :as log]
+            [jiksnu.actions.activity-actions :as actions.activity]
             [jiksnu.model.feed-source :as model.feed-source]
             [jiksnu.model.user :as model.user]
             [ring.util.response :as response])
-  (:import jiksnu.model.FeedSource))
+  (:import jiksnu.model.Activity
+           jiksnu.model.FeedSource))
 
 ;; process-updates
 
@@ -36,7 +39,14 @@
     (let [activity (if *dynamic*
                      (FeedSource.)
                      item)]
-      (show-section item))]
+      (list (show-section item)
+            (with-page "activities"
+              (let [page (actions.activity/fetch-by-feed-source item)
+                    items (if *dynamic* [(Activity.)] (:items page))]
+                (list
+                 (pagination-links (if *dynamic* {} page))
+                 [:div {:data-bind "with: items"}
+                  (index-section items)])))))]
    :viewmodel (str (named-path "show feed-source" {:id (:_id item)}) ".viewmodel")})
 
 (defview #'show :model
@@ -46,7 +56,11 @@
 (defview #'show :viewmodel
   [request item]
   {:body {:targetFeedSource (:_id item)
-          :title (:title item)}})
+          :title (:title item)
+          :pages {:activities
+                  (let [page (actions.activity/fetch-by-feed-source item
+                                                                    {:sort-clause {:updated 1}})]
+                    (format-page-info page))}}})
 
 ;; update
 
