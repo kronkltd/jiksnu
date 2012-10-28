1a49680d0a41348e81d1bf59a59ec5dfa49120f3
Daniel E. Renfer <duck@kronkltd.net>
2012-10-27 21:47:01 -0400

    create trigger

--- src/jiksnu/triggers/domain_triggers.clj
+++ src/jiksnu/triggers/domain_triggers.clj
@@ -3,8 +3,8 @@
   (:require [clojure.tools.logging :as log]
             [jiksnu.actions.domain-actions :as actions.domain]))
 
-(defn create-trigger
-  [action [domain-name] domain]
-  (actions.domain/discover domain))
+;; (defn create-trigger
+;;   [action [domain-name] domain]
+;;   (actions.domain/discover domain))
 
-(add-trigger! #'actions.domain/create   #'create-trigger)
+;; (add-trigger! #'actions.domain/create   #'create-trigger)
