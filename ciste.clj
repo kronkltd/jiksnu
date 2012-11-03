6c3e596183a6ceb8fec53ece872701579089b1af
Daniel E. Renfer <duck@kronkltd.net>
2012-11-02 22:29:49 -0400

    services and modules available

--- ciste.clj
+++ ciste.clj
@@ -2,4 +2,23 @@
  :modules ["jiksnu.commands"
            "jiksnu.factory"
            "jiksnu.workers"
-           "jiksnu.views"]}
+           "jiksnu.views"]
+ :modules-available
+ [
+  "jiksnu.commands"
+  "jiksnu.factory"
+  "jiksnu.workers"
+  "jiksnu.views"
+
+  ]
+
+ :services-available
+ [
+  "ciste.service.aleph"
+  "ciste.services.nrepl"
+  "ciste.service.swank"
+  "ciste.service.tigase"
+  "jiksnu.plugins.google-analytics"
+   
+  ]
+ }
