dcfd1a46e444fd1a1a64274d08e0907f06775073
Daniel E. Renfer <duck@kronkltd.net>
2012-12-02 11:10:40 -0500

    conversation transforms

--- test/jiksnu/transforms/conversation_transforms_test.clj
+++ test/jiksnu/transforms/conversation_transforms_test.clj
@@ -8,23 +8,28 @@
             [jiksnu.actions.domain-actions :as actions.domain]
             [jiksnu.actions.feed-source-actions :as actions.feed-source]
             [jiksnu.existance-helpers :as existance]
+            [jiksnu.model :as model]
             [jiksnu.model.conversation :as model.conversation]))
 
 (test-environment-fixture
 
  (fact "#'set-update-source"
-   (fact "when the update source is set"
-     (let [source (existance/a-feed-source-exists)
-           conversation (factory :conversation {:update-source (:_id source)})]
-       (set-update-source conversation) => conversation))
-   (fact "when the update source is not set"
-     (fact "and the source can be discovered"
-       (let [source (factory :feed-source)
-             conversation (-> (factory :conversation)
-                              (dissoc :update-source))
-             url (:url conversation)]
-         (set-update-source conversation) => (contains {:update-source .id.})
-         (provided
-           (actions.feed-source/discover-source anything) => {:_id .id.})))))
+   (let [source (existance/a-feed-source-exists)]
 
+     (fact "when the update source is set"
+       (let [conversation (factory :conversation {:update-source (:_id source)})]
+         (set-update-source conversation) => conversation))
+
+     (fact "when the update source is not set"
+       (fact "and the source can be discovered"
+         (let [params {:url (:topic source)
+                       ;; This is set by the other transform
+                       :_id (model/make-id)}
+               conversation (-> (factory :conversation params)
+                                (dissoc :update-source))
+               url (:url conversation)]
+           (set-update-source conversation) => (contains {:update-source .id.})
+           (provided
+             (actions.feed-source/discover-source anything) => {:_id .id.}))))
+     ))
  )
