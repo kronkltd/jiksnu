1f11d42781e27b211cc11fd51a70af998be55713
Daniel E. Renfer <duck@kronkltd.net>
2013-03-02 14:22:30 -0500

    describe config options

--- src/jiksnu/actions/setting_actions.clj
+++ src/jiksnu/actions/setting_actions.clj
@@ -1,5 +1,5 @@
 (ns jiksnu.actions.setting-actions
-  (:use [ciste.config :only [config set-config! write-config!]]
+  (:use [ciste.config :only [config describe-config set-config! write-config!]]
         [ciste.core :only [defaction]]
         [ciste.initializer :only [definitializer]]
         [ciste.loader :only [require-namespaces]])
@@ -7,8 +7,53 @@
 
 (defaction oauth-apps
   []
-  
+
   )
+
+(describe-config [:site :brought-by :name]
+  String
+  "The name of the organization running this service")
+
+(describe-config [:site :brought-by :url]
+  String
+  "The url of the organization running this service")
+
+(describe-config [:site :closed]
+  Boolean
+  "Can new users register?")
+
+(describe-config [:site :default :language]
+  String
+  "The default language to use")
+
+(describe-config [:site :default :timezone]
+  String
+  "The default timezone of this server")
+
+(describe-config [:site :email]
+  String
+  "The email address of the administrator")
+
+(describe-config [:site :invite-only]
+  Boolean
+  "Can users invite new users")
+
+(describe-config [:site :limit :text]
+  Number
+  "The maximum length of post to accept. (-1 = unlimited)")
+
+(describe-config [:site :name]
+  String
+  "The name of the service")
+
+(describe-config [:site :private]
+  Boolean
+  "Can unauthenticated users view this site?")
+
+(describe-config [:site :theme]
+  String
+  "The name of the theme to use")
+
 
 (defaction config-output
   []
@@ -54,7 +99,7 @@
               :enabled true
               :count 20
               :timespan 600
-              
+
               }
    :xmpp {
           :enabled true
