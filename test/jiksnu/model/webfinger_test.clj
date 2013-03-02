b893f2fff466c73d8ce40ee2c80b7fafd588cd10
Daniel E. Renfer <duck@kronkltd.net>
2013-03-02 15:41:50 -0500

    remove extra invalid test

--- test/jiksnu/model/webfinger_test.clj
+++ test/jiksnu/model/webfinger_test.clj
@@ -33,24 +33,6 @@
 
 (test-environment-fixture
 
- (fact "#'get-username-from-xrd"
-   (fact "when the usermeta has an identifier"
-     (get-username-from-xrd .user-meta.) => .username.
-     (provided
-       (get-username-from-identifiers .user-meta.) => .username.
-       (get-username-from-atom-property .user-meta.) => nil :times 0))
-   (fact "when the usermeta does not have an identifier"
-     (fact "and the atom link has an identifier"
-       (get-username-from-xrd .user-meta.) => .username.
-       (provided
-         (get-username-from-identifiers .user-meta.) => nil
-         (get-username-from-atom-property .user-meta.) => .username.))
-     (fact "and the atom link does not have an identifier"
-       (get-username-from-xrd .user-meta.) => nil
-       (provided
-         (get-username-from-identifiers .user-meta.) => nil
-         (get-username-from-atom-property .user-meta.) => nil))))
-
  (fact "#'get-username-from-atom-property"
    (fact "when the property has an identifier"
      (let [username (fseq :username)
