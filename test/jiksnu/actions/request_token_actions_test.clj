8fd19cce16796934fe049a2ef345bcb4865511ae
Daniel E. Renfer <duck@kronkltd.net>
2013-09-23 23:45:06 -0400

    checkpoint

--- test/jiksnu/actions/request_token_actions_test.clj
+++ test/jiksnu/actions/request_token_actions_test.clj
@@ -1,6 +1,7 @@
 (ns jiksnu.actions.request-token-actions-test
-  (:require [clj-factory.core :refer [factory]]
+  (:require [clj-factory.core :refer [factory fseq]]
             [jiksnu.actions.request-token-actions :as actions.request-token]
+            [jiksnu.factory :as factory]
             [jiksnu.mock :as mock]
             [jiksnu.session :as session]
             [jiksnu.test-helper :refer [check context future-context test-environment-fixture]]
@@ -13,14 +14,12 @@
 
  (context #'actions.request-token/create
    (let [client (mock/a-client-exists)
-         params {:client (:_id client)}]
+         params {:client (:_id client)
+                 :callback (fseq :uri)}]
      (actions.request-token/create params) =>
      (check [token]
        token => (partial instance? RequestToken)
-       (:_id token) => (partial instance? ObjectId)
-       )
-     )
-   )
+       (:_id token) => (partial instance? String))))
 
  ;; (context #'actions.request-token/get-request-token
  ;;   (let [params {}]
@@ -32,6 +31,3 @@
  ;;   )
 
  )
-
-
-
