43b95bed266cf7dad6e5e86db352acb87b039de8
Daniel E. Renfer <duck@kronkltd.net>
2013-09-27 22:35:16 -0400

    checkpoint

--- src-cljs/jiksnu/routes.cljs
+++ src-cljs/jiksnu/routes.cljs
@@ -23,12 +23,44 @@
         args-map (into {} args-array)]
     (array (str route) args-map)))
 
+(defn deparam
+  [param-string]
+  (let [result (js-obj)]
+    (when param-string
+      (.each js/$
+             (.split param-string "&")
+             (fn [index value]
+               (when value
+                 (let [param (.split value "=")]
+                   (aset result
+                         (aget param 0)
+                         (aget param 1)
+                         )
+                   )
+                 )
+               )
+             ))
+    result)
+  )
+
 (def Router
   (.extend backbone/Router
            (js-obj
             "routes" (js-obj "*actions" "defaultRoute")
             "defaultRoute" (fn [path-string]
-                             (log/fine *logger* "default route")
-                             (ws/send "fetch-viewmodel"
-                                      (parse-route path-string))))))
+                             (log/fine *logger* (format "default route: %s" path-string))
+                             (let [p (.-search (.-location js/document))]
+                               (ws/send "fetch-viewmodel"
+                                        (parse-route (jl/spy (str path-string p))))))
+            ;; "_extractParameters" (fn [route fragment]
+            ;;                        (log/fine "fragment")
+            ;;                        (jl/spy fragment)
+            ;;                        (let [result (jl/spy
+            ;;                                      (.slice
+            ;;                                       (.exec route fragment) 1))]
+            ;;                          (.unshift result
+            ;;                                    (deparam (aget result (dec (.-length result)))))
+            ;;                          (.slice result 0 -1)))
+
+            )))
 
