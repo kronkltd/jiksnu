2f3033756612871f2adb365573bd8606bc967c4b
Daniel E. Renfer <duck@kronkltd.net>
2014-03-31 22:48:57 -0400

    function stubs

--- src/jiksnu/routes/helpers.clj
+++ src/jiksnu/routes/helpers.clj
@@ -1,6 +1,11 @@
 (ns jiksnu.routes.helpers
   #_(:use [clojurewerkz.route-one.core :only [named-path named-url]]))
 
+(defn named-path
+  [& _])
+
+(defn add-route! [& _])
+
 (defn formatted-path
   ([name]
      (str #_(named-path name) ".:format"))
