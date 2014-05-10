3f339ff18d0c6991d9cb92296daeb2a527ff7a0c
Daniel E. Renfer <duck@kronkltd.net>
2014-05-10 09:43:28 -0400

    remove xmpp module

--- src/jiksnu/modules/xmpp/views.clj
+++
@@ -1,9 +0,0 @@
-(ns jiksnu.modules.xmpp.views
-  (:use [ciste.core :only [serialize-as]])
-  (:require [clj-tigase.core :as tigase]
-            [clojure.tools.logging :as log]))
-
-(defmethod serialize-as :xmpp
-  [serialization response]
-  (when response
-    (tigase/make-packet response)))
