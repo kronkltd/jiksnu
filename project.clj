f787f06062c00b1dcd502cfc548a80d331af449a
Daniel E. Renfer <duck@kronkltd.net>
2014-05-10 09:23:43 -0400

    change project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jiksnu-core "0.1.0-SNAPSHOT"
+(defproject net.kronkltd/jiksnu-core "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
