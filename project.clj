7e8dcf9794d15955cbdf9bd2bb5c7e60707f4720
Daniel E. Renfer <duck@kronkltd.net>
2014-11-02 12:03:24 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -3,29 +3,29 @@
   :url "https://github.com/duck1123/jiksnu"
   :author "Daniel E. Renfer <duck@kronkltd.net>"
   :min-lein-version "2.0.0"
-  :dependencies [[aleph "0.3.2"]
+  :dependencies [[aleph "0.3.3"]
                  [ciste "0.5.0-SNAPSHOT"]
                  [ciste/ciste-incubator "0.1.0-SNAPSHOT"
                   :exclusions [ciste/ciste-core]]
                  [ciste/ciste-service-aleph "0.4.0-SNAPSHOT"
                   :exclusions [ciste/ciste-core]]
                  [clj-factory "0.2.2-SNAPSHOT"]
-                 [clj-time "0.7.0"]
-                 [clj-http "0.9.2"]
+                 [clj-time "0.8.0"]
+                 [clj-http "1.0.1"]
                  [clj-webdriver "0.6.1"
                   :exclusions [xalan]]
                  [clojurewerkz/route-one "1.1.0"]
-                 [clojurewerkz/mailer "1.0.0"]
-                 [clojure-complete "0.2.3"
+                 [clojurewerkz/mailer "1.2.0"]
+                 [clojure-complete "0.2.4"
                   :exclusions [org.clojure/clojure]]
-                 [clojurewerkz/support "1.0.0"]
+                 [clojurewerkz/support "1.1.0"]
                  [com.novemberain/monger "1.8.0"]
                  [com.novemberain/validateur "1.7.0"]
                  [com.ocpsoft/ocpsoft-pretty-time "1.0.7"]
                  [crypto-random "1.2.0"]
                  [hiccup "1.0.5"]
-                 [im.chit/purnam "0.4.3"]
-                 [lamina "0.5.2"]
+                 [im.chit/purnam "0.5.1"]
+                 [lamina "0.5.5"]
                  [lolg "0.1.0-SNAPSHOT"
                   :exclusions [org.clojure/google-closure-library]]
                  [net.kronkltd/clj-airbrake "2.0.1-SNAPSHOT"]
@@ -37,12 +37,12 @@
                  [org.apache.abdera/abdera-client "1.1.3"]
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2261"]
-                 [org.clojure/core.cache "0.6.3"]
-                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/core.cache "0.6.4"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.reader "0.8.3"]
                  [org.clojure/data.json "0.2.4"]
-                 [org.jsoup/jsoup "1.7.3"]
+                 [org.jsoup/jsoup "1.8.1"]
                  [org.mindrot/jbcrypt "0.3m"]
                  [org.slf4j/slf4j-api "1.7.7"]
                  [org.slf4j/slf4j-log4j12 "1.7.7"]
@@ -52,12 +52,12 @@
                  [org.webjars/knockout "3.1.0"]
                  [org.webjars/momentjs "2.6.0-2"]
                  [org.webjars/underscorejs "1.6.0-3"]
-                 [prismatic/dommy "0.1.2"]
+                 [prismatic/dommy "1.0.0"]
                  [ring "1.2.2"]
                  [ring/ring-core "1.1.8"]
                  [ring-basic-authentication "1.0.5"]
                  [rmarianski/tidy-up "0.0.2"]
-                 [slingshot "0.10.3"]
+                 [slingshot "0.12.1"]
                  [xerces/xercesImpl "2.11.0"]
                  ;; I know you really want to upgrade this one, but
                  ;; you can't. :(
