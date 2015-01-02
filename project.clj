5c2d5d86226ee07667b19948b31c959749b54d52
Daniel E. Renfer <duck@kronkltd.net>
2015-01-02 14:38:15 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -4,33 +4,33 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [aleph "0.3.2"]
+                 [aleph "0.3.3"]
                  [ciste "0.5.0-SNAPSHOT"]
                  [ciste/ciste-incubator "0.1.0-SNAPSHOT"
                   :exclusions [ciste/ciste-core]]
                  [clj-factory "0.2.2-SNAPSHOT"]
                  [clj-stacktrace "0.2.8"]
-                 [clj-time "0.7.0"]
-                 [clj-http "0.9.2"]
+                 [clj-time "0.9.0"]
+                 [clj-http "1.0.1"]
                  [clojurewerkz/support "1.0.0"]
                  [com.novemberain/monger "1.7.0"]
                  [com.novemberain/validateur "1.7.0"]
                  [com.ocpsoft/ocpsoft-pretty-time "1.0.7"]
                  [crypto-random "1.2.0"]
                  [hiccup "1.0.5"]
-                 [lamina "0.5.2"]
-                 [lib-noir "0.8.4"]
+                 [lamina "0.5.5"]
+                 [lib-noir "0.9.5"]
                  [net.kronkltd/clj-gravatar "0.1.0-SNAPSHOT"]
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.cache "0.6.3"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.reader "0.8.3"]
+                 [org.clojure/core.cache "0.6.4"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.reader "0.8.13"]
                  [org.clojure/data.json "0.2.5"]
-                 [org.jsoup/jsoup "1.7.3"]
-                 [org.slf4j/slf4j-api "1.7.7"]
-                 [org.slf4j/slf4j-log4j12 "1.7.7"]
-                 [slingshot "0.10.3"]
+                 [org.jsoup/jsoup "1.8.1"]
+                 [org.slf4j/slf4j-api "1.7.9"]
+                 [org.slf4j/slf4j-log4j12 "1.7.9"]
+                 [slingshot "0.12.1"]
                  [clj-webdriver "0.6.1"]
                  [xerces/xercesImpl "2.11.0"]
                  ;; I know you really want to upgrade this one, but
@@ -42,8 +42,8 @@
               :dependencies
               [[midje         "1.6.3"]
                [ring-mock     "0.1.5"]]}}
-  :plugins [[codox          "0.6.1"]
-            [lein-midje     "3.0-beta1"]]
+  :plugins [[codox          "0.8.10"]
+            [lein-midje     "3.1.3"]]
 
   :aot [jiksnu.model]
   )
