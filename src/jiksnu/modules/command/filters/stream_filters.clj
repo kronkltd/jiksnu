e9d79869d9341300fa29b11128619ab9d29cb124
Daniel E. Renfer <duck@kronkltd.net>
2015-01-04 19:04:48 -0500

    factor out command module

--- src/jiksnu/modules/command/filters/stream_filters.clj
+++
@@ -1,9 +0,0 @@
-(ns jiksnu.modules.command.filters.stream-filters
-  (:require [ciste.filters :refer [deffilter]]
-            [clojure.tools.logging :as log]
-            [jiksnu.actions.stream-actions :as actions.stream]))
-
-(deffilter #'actions.stream/public-timeline :command
-  [action request]
-  (action))
-
