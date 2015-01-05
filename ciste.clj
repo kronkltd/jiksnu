01f2f67d37989a68bdf0a9a1efa796e7cac8efea
Daniel E. Renfer <duck@kronkltd.net>
2015-01-04 19:05:05 -0500

    remove available keys from ciste config

--- ciste.clj
+++ ciste.clj
@@ -3,14 +3,4 @@
  :modules ["jiksnu.core"
            "jiksnu.modules.core"
            "jiksnu.modules.web"]
-
- :modules-available []
-
- :services-available
- [
-  "ciste.service.aleph"
-  "ciste.services.nrepl"
-  "ciste.service.swank"
-  "jiksnu.plugins.google-analytics"
-  ]
  }
