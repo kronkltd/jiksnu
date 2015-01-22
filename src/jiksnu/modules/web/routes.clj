5877f6a50d46068f5e1383acd0b58224cdcd1621
Daniel E. Renfer <duck@kronkltd.net>
2015-01-21 21:40:18 -0500

    route nses

--- src/jiksnu/modules/web/routes.clj
+++ src/jiksnu/modules/web/routes.clj
@@ -3,16 +3,12 @@
             [ciste.initializer :refer [definitializer]]
             [ciste.middleware :as middleware]
             [ciste.routes :refer [resolve-routes]]
-            [clojure.data.json :as json]
             [clojure.tools.logging :as log]
-            [compojure.core :as compojure :refer [GET]]
-            [compojure.handler :as handler]
+            [compojure.core :refer [GET routes]]
             [compojure.route :as route]
-            [hiccup.core :as h]
             [jiksnu.actions.stream-actions :as stream]
             [jiksnu.model :as model]
             [jiksnu.modules.web.middleware :as jm]
-            [jiksnu.predicates :as predicates]
             [jiksnu.registry :as registry]
             [jiksnu.modules.http.routes :as r]
             [jiksnu.modules.web.helpers :as helpers]
@@ -61,13 +57,13 @@
                                  (set-site)))
 
   (def app
-    (compojure/routes
-     (compojure/GET "/" request
+    (routes
+     (GET "/" request
                     (when (:websocket? request)
                       ((http/wrap-aleph-handler stream/websocket-handler)
                        request)))
      (route/resources "/webjars/" {:root "META-INF/resources/webjars/"})
      (route/resources "/")
-     (compojure/GET "/main/events" [] stream/stream-handler)
+     (GET "/main/events" [] stream/stream-handler)
      (GET "/templates/*" [] #'helpers/serve-template)
      (wrap-trace #'site :header :ui))))
