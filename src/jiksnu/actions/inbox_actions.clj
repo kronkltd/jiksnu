648bac4e9b24f7f36d19241b79b909adf77c4214
Daniel E. Renfer <duck@kronkltd.net>
2015-02-08 11:47:43 -0500

    note unimplemented action

--- src/jiksnu/actions/inbox_actions.clj
+++ src/jiksnu/actions/inbox_actions.clj
@@ -1,7 +1,9 @@
 (ns jiksnu.actions.inbox-actions
-  (:use [ciste.core :only [defaction]])
-  (:require [jiksnu.model.item :as model.item]))
+  (:require [ciste.core :refer [defaction]]
+            [ciste.model :as cm]
+            [jiksnu.model.item :as model.item]))
 
 (defaction index
   [user]
+  (cm/implement)
   #_(model.item/fetch-activities user))
