c47ad90ff225ae64452a204d3ef0692781467e43
Daniel E. Renfer <duck@kronkltd.net>
2015-09-04 13:51:08 -0400

    add devel plugins

--- project.clj
+++ project.clj
@@ -37,4 +37,7 @@
                [ring-mock     "0.1.5"]
                [org.slf4j/slf4j-log4j12 "1.7.12"]]}}
   :plugins [[codox          "0.8.10"]
-            [lein-midje     "3.1.3"]])
+            [lein-midje     "3.1.3"]
+            [lein-ancient "0.6.7"]
+            [lein-bikeshed "0.2.0"]
+            ])
