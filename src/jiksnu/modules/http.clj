b586c723ecfca3674cfad0c9e42f1b884d095937
Daniel E. Renfer <duck@kronkltd.net>
2015-09-24 08:14:47 -0400

    core formats loading

--- src/jiksnu/modules/http.clj
+++ src/jiksnu/modules/http.clj
@@ -1,7 +1,12 @@
 (ns jiksnu.modules.http
   (:require [ciste.loader :as loader
              :refer [defhandler defmodule]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            ;; FIXME: If core is loaded properly, this is not needed
+            jiksnu.modules.core.formats
+            jiksnu.modules.core.views
+
+            ))
 
 (defn start
   []
