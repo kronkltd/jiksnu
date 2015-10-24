a074dfd250326eb9d3af7ab0ecad945b0d765431
Daniel E. Renfer <duck@kronkltd.net>
2015-10-24 12:44:41 -0400

    delete valueless views/filters

--- src/jiksnu/modules/web/views/access_token_views.clj
+++
@@ -1,9 +0,0 @@
-(ns jiksnu.modules.web.views.access-token-views
-  (:require [ciste.views :refer [defview]]
-            [jiksnu.actions.access-token-actions :as actions.access-token]))
-
-(defview #'actions.access-token/get-access-token :text
-  [request response]
-  {:body (format "oauth_token=%s&oauth_token_secret=%s"
-                 (:_id response)
-                 (:secret response))})
