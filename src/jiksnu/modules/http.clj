7a26e74c655007524bef5a211f64da35bb7fb3be
Daniel E. Renfer <duck@kronkltd.net>
2015-10-27 08:55:56 -0400

    convert to json-formatter

--- src/jiksnu/modules/http.clj
+++ src/jiksnu/modules/http.clj
@@ -1,6 +1,7 @@
 (ns jiksnu.modules.http
   (:require [active.timbre-logstash :refer [timbre-json-appender]]
             [ciste.loader :refer [defmodule]]
+            [clojure.data.json :as json]
             [clojure.tools.logging :as log]
             jiksnu.modules.core.formats
             jiksnu.modules.core.views
@@ -11,47 +12,53 @@
             [fipp.edn :refer (pprint)]
             [puget.printer :as puget]))
 
-(def json-appender
-  {:min-level nil
-   :enabled? true
-   :async? false
-   :rate-limit nil
-   :output-fn :inherit
-   :fn (fn [data]
-         (let [{:keys [instant level ?err_ varargs_
-                       output-fn config appender]} data
-               out-data {:err (force ?err_)
-                         :level level
-                         :file (:?file data)
-                         :instant instant
-                         :message (force (:msg_ data))
-                         :varargs (:varargs_ data)
-                         ;; :keys (keys data)
-                         :line (:?line data)
-                         :ns (:?ns-str data)
-                         :context (:context data)
-                         :hostname (force (:hostname_ data))}]
-           (println "")
-           (println "")
-           (->> out-data
-               (map (fn [[k v]] (when v [k v])))
-               (into {})
-               puget/cprint)))})
+(defn json-formatter
+  ([data] (json-formatter nil data))
+  ([opts data]
+   (let [{:keys [instant level ?err_ varargs_
+                 output-fn config appender]} data
+         out-data {:err (force ?err_)
+                   :level level
+                   :file (:?file data)
+                   :instant instant
+                   :message (force (:msg_ data))
+                   :varargs (:varargs_ data)
+                   ;; :keys (keys data)
+                   :line (:?line data)
+                   :ns (:?ns-str data)
+                   :context (:context data)
+                   :hostname (force (:hostname_ data))}]
+     ;; (println "")
+     ;; (println "")
+     (->> out-data
+          (map (fn [[k v]] (when v [k v])))
+          (into {})
+          ;; puget/cprint
+          json/json-str
+          ))))
 
 (defn start
   []
   (timbre/info "starting http")
   (timbre/info "before set")
-  (timbre/merge-config!
-   {:appenders
-    {:json json-appender
+  (timbre/set-config!
+   {:level :debug
+    :ns-whitelist []
+    :ns-blacklist []
+    :middleware []
+    :timestamp-opts timbre/default-timestamp-opts
+    :output-fn json-formatter
+    :appenders
+    {
+     ;; :json json-appender
      ;; :logstash (timbre-json-appender "192.168.1.151" 4660)
      ;; :logstash sr/logstash-appender
-     ;; :spit (spit-appender)
-     ;; :println (println-appender {:stream :auto})
+     :spit (spit-appender)
+     :println (println-appender {:stream :auto})
      }
     :shared-appender-config {:logstash {:port 4660 :logstash "192.168.1.151"}}})
-  (timbre/info "after set"))
+  (timbre/with-context {:foo "bar"}
+    (timbre/info "after set")))
 
 (defmodule "http"
   :start start
