c3ffe16cd6b648477eeaccc6e234ab16a765e294
Daniel E. Renfer <duck@kronkltd.net>
2015-10-31 12:27:41 -0400

    groups test

---
+++ test/jiksnu/modules/web/routes/group_routes_test.clj
@@ -0,0 +1,48 @@
+(ns jiksnu.modules.web.routes.group-routes-test
+  (:require [ciste.model :as cm]
+            [ciste.sections.default :refer [uri]]
+            [clj-factory.core :refer [factory]]
+            [clojure.data.json :as json]
+            [clojurewerkz.support.http.statuses :as status]
+            [hiccup.core :as h]
+            [jiksnu.actions.domain-actions :as actions.domain]
+            [jiksnu.mock :as mock]
+            [jiksnu.model :as model]
+            [jiksnu.model.activity :as model.activity]
+            [jiksnu.model.domain :as model.domain]
+            [jiksnu.routes-helper :refer [response-for]]
+            [jiksnu.test-helper :as th]
+            [jiksnu.util :as util]
+            [midje.sweet :refer :all]
+            [ring.mock.request :as req]))
+
+(namespace-state-changes
+ [(before :contents (th/setup-testing))
+  (after :contents (th/stop-testing))])
+
+(facts "route: group-api/collection :get"
+  (let [url (str "/model/conversations")
+        request (req/request :get url)]
+    (response-for request) =>
+    (contains
+     {:status 200})))
+
+(facts "route: group-api/collection :post"
+  (let [params (factory :group)
+        url (str "/model/conversations")
+        request (assoc (req/request :post url)
+                       :non-query-params params)]
+    (prn params)
+
+    (response-for request) =>
+    (contains
+     {:status 201})))
+
+
+(facts "route: group-api/item :get"
+  (let [group (mock/a-group-exists)
+        url (str "/model/conversations/" (:_id group))
+        request (req/request :get url)]
+    (response-for request) =>
+    (contains
+     {:status 200})))
