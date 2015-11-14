5861d828a2695581729c8ed788ce7ffea0bdd0f6
Daniel E. Renfer <duck@kronkltd.net>
2015-11-14 12:43:54 -0500

    switch to ciste snapshot

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [ciste "0.6.0-alpha1"
+                 [ciste "0.6.0-SNAPSHOT"
                   :exclusions [ring/ring-core
                                org.clojure/tools.reader
                                org.clojure/clojurescript
