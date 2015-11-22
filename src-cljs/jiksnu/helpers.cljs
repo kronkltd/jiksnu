182c1661fd952f57beabb18d59b3c45383721fd8
Daniel E. Renfer <duck@kronkltd.net>
2015-11-21 22:26:15 -0500

    align hotkeys

--- src-cljs/jiksnu/helpers.cljs
+++ src-cljs/jiksnu/helpers.cljs
@@ -158,7 +158,7 @@
 (defn setup-hotkeys
   [hotkeys $state]
   (state-hotkey "g d" "indexDomains" "Go to Domains")
-  (state-hotkey "g g" "indexGroups" "Go to Groups")
-  (state-hotkey "g h" "home" "Go to Home")
+  (state-hotkey "g g" "indexGroups"  "Go to Groups")
+  (state-hotkey "g h" "home"         "Go to Home")
   (state-hotkey "g s" "settingsPage" "Go to Settings")
-  (state-hotkey "g u" "indexUsers" "Go to Users"))
+  (state-hotkey "g u" "indexUsers"   "Go to Users"))
