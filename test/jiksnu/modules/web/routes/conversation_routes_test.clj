db7c14e52a062e1b680837eadd1852707b044e4d
Daniel E. Renfer <duck@kronkltd.net>
2016-02-07 01:00:26 -0500

    checkpoint

--- test/jiksnu/modules/web/routes/conversation_routes_test.clj
+++ test/jiksnu/modules/web/routes/conversation_routes_test.clj
@@ -1,8 +1,9 @@
 (ns jiksnu.modules.web.routes.conversation-routes-test
-  (:require [ciste.sections.default :refer [full-uri]]
+  (:require [clojure.tools.logging :as log]
             [jiksnu.mock :as mock]
             [jiksnu.routes-helper :refer [as-user response-for]]
             [jiksnu.test-helper :as th]
+            [jiksnu.util :as util]
             [midje.sweet :refer :all]
             [ring.mock.request :as req]))
 
@@ -13,7 +14,13 @@
 (facts "route: converation-api/activities-stream :get"
   (let [conversation (mock/a-conversation-exists)
         url (str "/model/conversations/" (:_id conversation) "/activities")
-        request (req/request :get url)]
-    (response-for request) =>
-    (contains
-     {:status 200})))
+        request (req/request :get (log/spy :info url))]
+    (util/inspect request)
+
+    (mock/there-is-an-activity {:conversation conversation})
+
+    (let [response (response-for request)]
+      (util/inspect response)
+      response =>
+      (contains
+       {:status 200}))))
