ea76dc7d4f1bb88c3fd8726d78c0abdd9214c249
Daniel E. Renfer <duck@kronkltd.net>
2016-02-14 01:01:14 -0500

    remove control lines

--- src/jiksnu/modules/web/sections.clj
+++ src/jiksnu/modules/web/sections.clj
@@ -84,19 +84,6 @@
         property
         " : ''")})
 
-(defn control-line
-  [label name type & {:as options}]
-  (let [{:keys [value checked]} options]
-    [:div.control-group
-     [:label.control-label {:for name} label]
-     [:div.controls
-      [:input
-       (merge {:type type :name name}
-              (when value
-                {:value value})
-              (when checked
-                {:checked "checked"}))]]]))
-
 (defn dropdown-menu
   [item buttons]
   (when (seq buttons)
