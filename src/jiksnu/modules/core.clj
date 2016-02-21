1fbe098297f0cf2d14bdda6ab195c0d482d5a5ef
Daniel E. Renfer <duck@kronkltd.net>
2016-02-21 15:30:45 -0500

    remove jiksnu.core module

--- src/jiksnu/modules/core.clj
+++ src/jiksnu/modules/core.clj
@@ -2,49 +2,69 @@
   (:require [ciste.event :as event]
             [ciste.loader :refer [defmodule]]
             [clojurewerkz.eep.emitter :refer [defobserver delete-handler get-handler]]
+            [jiksnu.actions.activity-actions :as actions.activity]
+            [jiksnu.actions.like-actions :as actions.like]
             [jiksnu.actions.stream-actions :as actions.stream]
+            [jiksnu.actions.subscription-actions :as actions.subscription]
+            [jiksnu.actions.user-actions :as actions.user]
+            [jiksnu.channels :as ch]
+            [jiksnu.db :as db]
+            [jiksnu.model.feed-source :as model.feed-source]
+            [jiksnu.model.user :as model.user]
             jiksnu.modules.core.formats
+            jiksnu.modules.core.triggers.activity-triggers
+            jiksnu.modules.core.triggers.conversation-triggers
             [jiksnu.modules.core.triggers.domain-triggers :as triggers.domain]
             jiksnu.modules.core.views
             [jiksnu.templates.model :as templates.model]
-            [jiksnu.db :as db]
             [jiksnu.registry :as registry]
             [jiksnu.util :as util]
+            [manifold.bus :as bus]
+            [manifold.stream :as s]
             [taoensso.timbre :as timbre]))
-
-(def handlers (ref []))
 
 (defn start
   []
   (db/set-database!)
+  ;; (model.activity/ensure-indexes)
+  (model.feed-source/ensure-indexes)
+  (model.user/ensure-indexes)
+
+  ;; cascade delete on domain deletion
+  (dosync
+   (alter actions.user/delete-hooks conj #'actions.activity/handle-delete-hook))
+
+  (actions.subscription/setup-delete-hooks)
+
+  (->> (bus/subscribe ch/events :activity-posted)
+       (s/consume actions.subscription/handle-follow-activity))
+
+  (->> (bus/subscribe ch/events :activity-posted)
+       (s/consume actions.like/handle-like-activity))
 
   (triggers.domain/init-receivers)
 
-  (let [emitter
-        (defobserver event/emitter ::templates.model/item-created
-          (fn [{:keys [collection-name event item]}]
-            (condp = collection-name
+  (defobserver event/emitter ::templates.model/item-created
+    (fn [{:keys [collection-name event item]}]
+      (condp = collection-name
 
-              "activities"
-              (do
-                (timbre/info "activity created")
-                (condp = (:verb item)
-                  (do
-                    (timbre/info "Unknown verb")
-                    (util/inspect item))))
+        "activities"
+        (do
+          (timbre/info "activity created")
+          (condp = (:verb item)
+            (do
+              (timbre/info "Unknown verb")
+              (util/inspect item))))
 
-              "users"
-              (do
-                (timbre/info "user created")
-                (actions.stream/add-stream (util/inspect item) "* major")
-                (actions.stream/add-stream item "* minor"))
+        "users"
+        (do
+          (timbre/info "user created")
+          (actions.stream/add-stream (util/inspect item) "* major")
+          (actions.stream/add-stream item "* minor"))
 
-              (do
-                (timbre/infof "Other created - %s" collection-name)
-                #_(util/inspect item)))))]
-
-    (dosync
-     (alter handlers conj emitter)))
+        (do
+          (timbre/infof "Other created - %s" collection-name)
+          #_(util/inspect item)))))
 
   ;; (defobserver event/emitter ::templates.model/item-created
   ;;   (fn [{:keys [collection-name event item]}]
@@ -57,10 +77,7 @@
 (defn stop
   []
   (timbre/info "Stopping core")
-
-  (get-handler event/emitter ::templates.model/item-created)
-  (delete-handler event/emitter ::templates.model/item-created)
-  (get-handler event/emitter ::templates.model/item-created))
+  (delete-handler event/emitter ::templates.model/item-created))
 
 (def module
   {:name "jiksnu.modules.core"
