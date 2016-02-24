1b4cc3d01500257460baa81d74f314e43aaf4684
Daniel E. Renfer <duck@kronkltd.net>
2016-02-24 01:41:50 -0500

    re-enable phantomjs testing

--- karma.conf.js
+++ karma.conf.js
@@ -92,7 +92,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
     browsers: [
-    // 'PhantomJS'
+    'PhantomJS'
     ],
 
     // If browser does not capture in given timeout [ms], kill it
