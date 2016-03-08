ae7f7748ed57ce72f60eef412ddae116d5751129
Daniel E. Renfer <duck@kronkltd.net>
2016-03-07 22:10:04 -0500

    default config options

--- profiles.clj
+++ profiles.clj
@@ -1,6 +1,16 @@
 {
- :user-dev  {:env {:ciste-env "dev"
-                   :jiksnu-db-url "mongodb://localhost/jiksnu_dev"}}
- :user-test {:env {:ciste-env "test"
-                   :jiksnu-db-url "mongodb://localhost/jiksnu_test"}}
+ :user-dev
+ {:env
+  {
+   :ciste-env "dev"
+   :domain "dev.jiksnu.com"
+   :jiksnu-db-url "mongodb://localhost/dev.jiksnu.com"
+   :jiksnu-http-secure true
+   }}
+ :user-test
+ {:env
+  {
+   :ciste-env "test"
+   :jiksnu-db-url "mongodb://localhost/jiksnu_test"
+   }}
  }
