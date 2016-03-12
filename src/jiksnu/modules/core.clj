0af40473f8984f9aa2144e6b52ab22083c41ce40
Daniel E. Renfer <duck@kronkltd.net>
2016-03-12 18:05:06 -0500

    turn off observer

--- src/jiksnu/modules/core.clj
+++ src/jiksnu/modules/core.clj
@@ -88,7 +88,7 @@
 
   (util/inspect event/emitter)
 
-  (defobserver event/emitter ::templates.model/item-created handle-created)
+  ;; (defobserver event/emitter ::templates.model/item-created handle-created)
 
   ;; (defobserver event/emitter ::templates.model/item-created
   ;;   (fn [{:keys [collection-name event item]}]
