8f6ad8e3e129908ad30df144644157520d8b9073
Daniel E. Renfer <duck@kronkltd.net>
2016-04-17 22:23:14 -0400

    set database defaults

--- src/jiksnu/db.clj
+++ src/jiksnu/db.clj
@@ -1,5 +1,5 @@
 (ns jiksnu.db
-  (:require [ciste.config :refer [config config*]]
+  (:require [ciste.config :refer [config config* describe-config]]
             [inflections.core :as inf]
             [monger.collection :as mc]
             [monger.core :as mg]
@@ -9,6 +9,25 @@
 
 (def _db (ref nil))
 (def _conn (ref nil))
+
+(describe-config [:jiksnu :db :host]
+  String
+  "The mongodb host"
+  :default "localhost")
+
+(describe-config [:jiksnu :db :name]
+  String
+  "The mongodb collection"
+  :default "jiksnu")
+
+(describe-config [:jiksnu :db :port]
+  Integer
+  "The mongodb port"
+  :default 27017)
+
+(describe-config [:jiksnu :db :url]
+  String
+  "The mongodb connection info as a uri")
 
 ;; Database functions
 
