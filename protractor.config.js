19e109f55be9eb8d3d15904a4ada8db02f2218d7
Daniel E. Renfer <duck@kronkltd.net>
2016-05-05 22:09:22 -0400

    remove old protractor config

--- protractor.config.js
+++ protractor.config.js
@@ -14,25 +14,3 @@ config.plugins = [({package: 'protractor-console',
                     logLevels: ['debug', 'info', 'warning', 'severe', 'log']})];
 
 exports.config = config;
-
-// exports.config = {
-//   // Uncomment this line if running with a separate webdriver server
-//   seleniumAddress: "http://localhost:4444/wd/hub",
-//   capabilities: {
-//     browserName: 'phantomjs',
-//     version: '',
-//     platform: 'ANY'
-//   },
-//   framework: 'custom',
-//   frameworkPath: require.resolve('protractor-cucumber-framework'),
-//   baseUrl: 'http://localhost:8080/',
-//   allScriptsTimeout: 11000,
-//   getPageTimeout: 10000,
-//   specs: [
-//     'features/*.feature'
-//   ],
-//   cucumberOpts: {
-//     require: 'target/protractor-tests.js',
-//     format: 'summary'
-//   }
-// };
