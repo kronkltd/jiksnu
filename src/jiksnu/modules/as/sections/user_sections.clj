6df2115fe4b0a64084b7207ceb0629716afc99b8
Daniel E. Renfer <duck@kronkltd.net>
2016-10-16 00:44:23 -0400

    customizable scheme

--- src/jiksnu/modules/as/sections/user_sections.clj
+++ src/jiksnu/modules/as/sections/user_sections.clj
@@ -3,33 +3,34 @@
             [ciste.core :refer [with-context]]
             [ciste.sections :refer [defsection]]
             [ciste.sections.default :refer [show-section]]
+            [jiksnu.model.domain :as model.domain]
             [jiksnu.model.user :as model.user])
   (:import jiksnu.model.User))
 
 ;; show-section
 
-(def url-pattern       "https://%s/%s")
-(def profile-pattern   "https://%s/api/user/%s/profile")
-(def inbox-pattern     "https://%s/api/user/%s/inbox")
-(def outbox-pattern    "https://%s/api/user/%s/feed")
-(def followers-pattern "https://%s/api/user/%s/followers")
-(def following-pattern "https://%s/api/user/%s/following")
-(def favorites-pattern "https://%s/api/user/%s/favorites")
-(def lists-pattern     "https://%s/api/user/%s/lists/person")
+(def url-pattern       "%s://%s/main/users/%s")
+(def profile-pattern   "%s://%s/api/user/%s/profile")
+(def inbox-pattern     "%s://%s/api/user/%s/inbox")
+(def outbox-pattern    "%s://%s/api/user/%s/feed")
+(def followers-pattern "%s://%s/api/user/%s/followers")
+(def following-pattern "%s://%s/api/user/%s/following")
+(def favorites-pattern "%s://%s/api/user/%s/favorites")
+(def lists-pattern     "%s://%s/api/user/%s/lists/person")
 
 (defsection show-section [User :as]
   [user & options]
-  (let [{:keys [display-name id avatar-url
-                domain username]} user
+  (let [{:keys [display-name id avatar-url domain username]} user
+        scheme (if (some-> domain model.domain/fetch-by-id :secure) "https" "http")
         avatar-url (or avatar-url (model.user/image-link user))
-        url           (format url-pattern domain username)
-        profile-url   (format profile-pattern   domain username)
-        inbox-url     (format inbox-pattern     domain username)
-        outbox-url    (format outbox-pattern    domain username)
-        followers-url (format followers-pattern domain username)
-        following-url (format following-pattern domain username)
-        favorites-url (format favorites-pattern domain username)
-        list-url      (format lists-pattern     domain username)]
+        url           (format url-pattern       scheme domain username)
+        profile-url   (format profile-pattern   scheme domain username)
+        inbox-url     (format inbox-pattern     scheme domain username)
+        outbox-url    (format outbox-pattern    scheme domain username)
+        followers-url (format followers-pattern scheme domain username)
+        following-url (format following-pattern scheme domain username)
+        favorites-url (format favorites-pattern scheme domain username)
+        list-url      (format lists-pattern     scheme domain username)]
     (array-map
      :preferredUsername username
      :url url
@@ -57,7 +58,8 @@
 
 (defn format-collection
   [user page]
-  (let [domain (config :domain)]
+  (let [domain (config :domain)
+        scheme (if (some-> domain model.domain/fetch-by-id :secure) "https" "http")]
     (with-context [:http :as]
       {:displayName (str "Collections of persons for " (:_id user))
        :objectTypes [(:objectTypes page "collection")]
