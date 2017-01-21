02ad81ec610fb2b747f790d5d2ad05605894ffb4
Daniel E. Renfer <duck@kronkltd.net>
2017-01-21 13:04:05 -0500

    Extract fetchers from page services

--- src-cljs/jiksnu/services.cljs
+++ src-cljs/jiksnu/services.cljs
@@ -4,41 +4,31 @@
             [taoensso.timbre :as timbre])
   (:use-macros [gyr.core :only [def.service]]))
 
+(defn fetch-page
+  [$http page-name]
+  (if-let [url (get registry/page-mappings page-name)]
+    ;; TODO: cache the page response here and you if-modified for updates
+    (-> (.get $http url)
+        (.then (fn [response] (.-data response))))
+    (throw (str "page mapping not defined: " page-name))))
+
+(defn fetch-sub-page
+  [$http parent page-name]
+  (let [type (.getType parent)]
+    (when-let [mapping-fn (get-in registry/subpage-mappings [type page-name])]
+      (let [url (mapping-fn parent)]
+        (-> (.get $http url )
+            (.then (fn [response] (.-data response))))))))
+
 (defn pageService
   "Angular service for retrieving pages"
   [$http]
-  (let [service #js {}]
-    (set! (.-fetch service)
-          (fn [page-name]
-            (if-let [url (get registry/page-mappings page-name)]
-              ;; TODO: cache the page response here and you if-modified for updates
-              (-> (.get $http url)
-                  (.then (fn [response] (.-data response))))
-              (throw (str "page mapping not defined: " page-name)))))
-    service))
-
-(set! (.-$inject pageService) #js ["$http"])
-(.service jiksnu "pageService" pageService)
+  #js {:fetch #(fetch-page $http %)})
 
 (defn subpageService
   "Angular service for retrieving subpages"
-  [$q $http]
-  (let [service #js {}]
-    (set! (.-fetch service)
-          (fn [parent page-name]
-            (let [type (.getType parent)
-                  d (.defer $q)]
-              (if-let [mapping-fn (get-in registry/subpage-mappings [type page-name])]
-                (let [url (mapping-fn parent)]
-                  ;; (timbre/debugf "url: %s" url)
-                  (-> $http
-                      (.get url)
-                      (.success #(.resolve d %))
-                      (.error #(.reject d)))
-                  (.-promise d))
-                (throw (str "Could not find subpage mapping for model "
-                            (type parent) " with label " page-name))))))
-    service))
+  [$http]
+  #js {:fetch #(fetch-sub-page $http %1 %2)})
 
-(set! (.-$inject subpageService) #js ["$q" "$http"])
-(.service jiksnu "subpageService" subpageService)
+(.service jiksnu "pageService" #js ["$http" pageService])
+(.service jiksnu "subpageService" #js ["$http" subpageService])
