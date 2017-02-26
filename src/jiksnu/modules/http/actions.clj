ce7270d72b97fbfb46e62301f9892746f74cc314
Daniel E. Renfer <duck@kronkltd.net>
2017-02-26 07:58:43 -0500

    Handle websocket registration

--- src/jiksnu/modules/http/actions.clj
+++ src/jiksnu/modules/http/actions.clj
@@ -35,14 +35,27 @@
                    :name "public-timeline"
                    :body (:_id item)}))
 
-(defn handle-closed
-  [channel status message]
-  (let [user-id (:_id (:user status))
-        connection-id (:connection status)]
-    (timbre/with-context {:user-id user-id :connection-id connection-id}
-      (timbre/info "closed connection"))
+
+(defn log-connections
+  []
+  (doseq [id (keys @connections)]
+    (timbre/debugf "%s => %s" id (keys (get @connections id)))))
+
+(defn deregister-connection!
+  "Adds a connection to the registry"
+  [{:keys [::user-id ::connection-id] :as status}]
+  (let [stream (get-in @connections [user-id connection-id])]
+    (timbre/debugf "deregistering connection %s(%s) => %s" user-id connection-id stream)
+
     (dosync
-     (alter connections #(update % user-id dissoc connection-id)))))
+     (alter connections #(update % user-id dissoc connection-id))))
+  (log-connections))
+
+(defn register-connection!
+  [{:keys [::user-id ::connection-id] :as status} response-channel]
+  (dosync
+   (alter connections #(assoc-in % [user-id connection-id] response-channel)))
+  (log-connections))
 
 (defaction alert-all
   [message]
@@ -50,31 +63,44 @@
     (let [response (json/write-str {:action "add notice" :message message})]
       (s/put! ch response))))
 
+(defn on-receive
+  [ch request response-channel body]
+  (when-let [resp (actions.stream/handle-command request response-channel body)]
+    (server/send! ch resp)))
+
+(defn handle-closed
+  [channel status message]
+  (let [user-id (::user-id status)
+        connection-id (::connection-id status)]
+    (timbre/with-context status
+      (timbre/debugf "Websocket connection closed. %s => %s" user-id connection-id))
+    (deregister-connection! status)))
+
 (defn connect
   [request ch]
   ;; (trace/trace :websocket:connections:established 1)
   (let [user-id (:current friend/*identity*)
         connection-id (util/new-id)
-        status {:user user-id :connection connection-id}
-        response-channel (s/stream)]
-    (timbre/with-context {:status (prn-str status)}
-      (timbre/infof "Websocket connection opened - %s" user-id))
+        status {::user-id user-id ::connection-id connection-id}
+        response-channel (s/stream* {:description connection-id})]
 
-    (dosync
-     (alter connections #(assoc-in % [user-id connection-id] response-channel)))
+    (timbre/with-context {:status (prn-str status)}
+      (timbre/debugf "Websocket connection opened. %s => %s" user-id connection-id))
+
+    (register-connection! status response-channel)
 
     (event/notify :connection-opened status)
 
-    (server/send! ch (json/write-str {:connection connection-id}))
+    (doto ch
 
-    ;; Executes commands for each input
-    (server/on-receive ch
-                       (fn [body]
-                         (when-let [resp (actions.stream/handle-command
-                                          request
-                                          response-channel body)]
-                           (server/send! ch resp))))
-    (server/on-close ch #(handle-closed response-channel status %))
+      ;; Acknowledge connection
+      (server/send! (json/write-str {::connection connection-id}))
+
+      ;; Message handler
+      (server/on-receive #(on-receive ch request response-channel %))
+
+      ;; Executes commands for each input
+      (server/on-close   #(handle-closed response-channel status %)))
 
     ;; Send posted activities to connected clients
     (s/connect
