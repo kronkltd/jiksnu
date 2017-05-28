40bfc241523f879d7124c675744e5fe3e286f276
Daniel E. Renfer <duck@kronkltd.net>
2017-05-28 11:07:03 -0400

    http logging

--- src-cljs/jiksnu/config.cljs
+++ src-cljs/jiksnu/config.cljs
@@ -17,10 +17,18 @@
 
   (js/angular.extend (.-defaults DSProvider)
                      #js {:idAttribute "_id"
-                          :basePath    "/model"})
+                          :basePath    "/model"
+                          :afterFind (fn [Resource data cb]
+                                       (timbre/debugf "data: %s" (js/JSON.stringify data))
+                                       (cb nil data))
+                          :afterFindAll (fn [Resource data cb]
+                                          (timbre/debugf "data: %s" (js/JSON.stringify data))
+                                          (cb nil data))})
 
-  (js/angular.extend (.-defaults DSHttpAdapterProvider)
-                     #js {:log false})
+  ;; (js/angular.extend (.-defaults DSHttpAdapterProvider)
+  ;;                    #js {:log false})
+
+
 
   (.otherwise $urlRouterProvider "/")
   (-> $locationProvider
