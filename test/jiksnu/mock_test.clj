25f10c672a307b021e83893b1d819bd83d200f29
Daniel E. Renfer <duck@kronkltd.net>
2017-07-04 10:22:12 -0400

    reformat mock test

--- test/jiksnu/mock_test.clj
+++ test/jiksnu/mock_test.clj
@@ -23,54 +23,54 @@
 (th/module-test ["jiksnu.modules.core"])
 
 (fact "#'mock/activity-gets-posted"
-  (mock/activity-gets-posted) => (partial instance? Activity))
+  (mock/activity-gets-posted)       => (partial instance? Activity))
 
 (fact "#'mock/a-domain-exists"
-  (mock/a-domain-exists) => (partial instance? Domain))
+  (mock/a-domain-exists)            => (partial instance? Domain))
 
 (fact "#'mock/a-resource-exists"
-  (mock/a-resource-exists) => (partial instance? Resource))
+  (mock/a-resource-exists)          => (partial instance? Resource))
 
 (fact "#'mock/a-client-exists"
-  (mock/a-client-exists) => (partial instance? Client))
+  (mock/a-client-exists)            => (partial instance? Client))
 
 (fact "#'mock/a-like-exists"
-  (mock/a-like-exists) => (partial instance? Like))
+  (mock/a-like-exists)              => (partial instance? Like))
 
 (fact "#'mock/a-notification-exists"
-  (mock/a-notification-exists) => (partial instance? Notification))
+  (mock/a-notification-exists)      => (partial instance? Notification))
 
 (fact "#'mock/a-user-exists"
   (fact "without any params"
-    (mock/a-user-exists) => (partial instance? User)))
+    (mock/a-user-exists)            => (partial instance? User)))
 
 (fact "#'mock/a-stream-exists"
   (fact "without any params"
-    (mock/a-stream-exists) => (partial instance? Stream)))
+    (mock/a-stream-exists)          => (partial instance? Stream)))
 
 (fact "#'mock/a-feed-source-exists"
-  (mock/a-feed-source-exists) => (partial instance? FeedSource))
+  (mock/a-feed-source-exists)       => (partial instance? FeedSource))
 
 (fact "#'mock/a-service-exists"
-  (mock/a-service-exists) => (partial instance? Service))
+  (mock/a-service-exists)           => (partial instance? Service))
 
 (fact "#'mock/a-conversation-exists"
-  (mock/a-conversation-exists) => (partial instance? Conversation))
+  (mock/a-conversation-exists)      => (partial instance? Conversation))
 
 (fact "#'mock/a-feed-subscription-exists"
   (mock/a-feed-subscription-exists) => (partial instance? FeedSubscription))
 
 (fact "#'mock/a-subscription-exists"
-  (mock/a-subscription-exists) => (partial instance? Subscription))
+  (mock/a-subscription-exists)      => (partial instance? Subscription))
 
 (fact "#'mock/a-group-exists"
-  (mock/a-group-exists) => (partial instance? Group))
+  (mock/a-group-exists)             => (partial instance? Group))
 
 (fact "#'mock/an-album-exists"
-  (mock/an-album-exists) => (partial instance? Album))
+  (mock/an-album-exists)            => (partial instance? Album))
 
 (fact "#'mock/a-request-token-exists"
-  (mock/a-request-token-exists) => (partial instance? RequestToken))
+  (mock/a-request-token-exists)     => (partial instance? RequestToken))
 
 (fact "#'mock/a-picture-exists"
-  (mock/a-picture-exists) => (partial instance? Picture))
+  (mock/a-picture-exists)           => (partial instance? Picture))
