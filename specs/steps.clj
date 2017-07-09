fbed288a3d0eb7d752d0345489e62bca772e9d6c
Daniel E. Renfer <duck@kronkltd.net>
2017-07-09 01:43:38 -0400

    register user step

--- specs/steps.clj
+++ specs/steps.clj
@@ -1,8 +1,12 @@
+(require '[jiksnu.helpers.actions :as helpers.action])
 (require '[jiksnu.helpers.features :as helpers.features])
 (require '[jiksnu.helpers.navigation :as helpers.navigation])
 
 (Before [] (helpers.features/before-hook))
 (After [] (helpers.features/after-hook))
+
+(Given #"^a user exists with the password \"(.*?)\"$" [password]
+  (helpers.action/register-user password))
 
 (When #"^I go to the \"([^\"]*)\" page$" [page-name]
   (helpers.navigation/go-to-the-page page-name))
