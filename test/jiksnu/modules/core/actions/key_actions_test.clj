5103dbb04b6357497ff7dd2ad20745990f330be9
Daniel E. Renfer <duck@kronkltd.net>
2017-07-30 11:30:58 -0400

    delete tests

--- test/jiksnu/modules/core/actions/key_actions_test.clj
+++
@@ -1,9 +0,0 @@
-(ns jiksnu.modules.core.actions.key-actions-test
-  (:require [jiksnu.modules.core.actions.key-actions :as actions.key]
-            [jiksnu.test-helper :as th]
-            [midje.sweet :refer :all]))
-
-(th/module-test ["jiksnu.modules.core"])
-
-(fact #'actions.key/index
-  (actions.key/index) => map?)
